473d2d6c9b8f469c514c4f7039f8d422cae0bfe1
Hoody <124743189+devhoody@users.noreply.github.com>
2023-03-20 17:58:21 +0900

    [level 0] Title: 나머지 구하기, Time: 0.02 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120810. 나머지 구하기/나머지 구하기.java
+++ 프로그래머스/lv0/120810. 나머지 구하기/나머지 구하기.java
@@ -1,6 +1,8 @@
 class Solution {
     public int solution(int num1, int num2) {
-        int answer = num1 % num2;
-        return answer;
+        while (num1 >= num2) {
+            num1 = num1 - num2;
+        }
+        return num1;
     }
 }
