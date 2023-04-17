3d913ce8d4ec1fc4781df56652dfe4a086c0b201
Hoody <124743189+devhoody@users.noreply.github.com>
2023-04-17 21:13:53 +0900

    [Bronze V] Title: 영수증, Time: 228 ms, Memory: 17808 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int X = sc.nextInt();
+        
+        int N = sc.nextInt();
+        
+        int a = 0;
+        int b = 0;
+        int sum = 0;
+        
+        for(int i=0; i<N; i++){
+            a = sc.nextInt();
+            b = sc.nextInt();
+            
+            sum += a * b;
+        }
+        
+        if(X == sum) {
+            System.out.println("Yes");
+        } else
+            System.out.println("No");
+    }
+}
