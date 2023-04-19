63338b846ce5eb6f91ff76794eb158ce1d2a6536
Hoody <124743189+devhoody@users.noreply.github.com>
2023-04-19 11:16:20 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 908 ms, Memory: 236856 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -1,12 +1,12 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class Main{
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
@@ -14,14 +14,14 @@ public class Main{
         
         StringTokenizer st;
         
-        for (int i=0; i<N; i++) {
+        for(int i=0; i<N; i++) {
             st = new StringTokenizer(br.readLine());
             
-            bw.write((Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()))+ "\n");
+            bw.write((Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()))+"\n");
         }
         br.close();
         
         bw.flush();
         bw.close();
-    }
+    }   
 }
