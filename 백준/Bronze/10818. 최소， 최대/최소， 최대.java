01bcde8ba768d44b5a9c16d298570de130aca312
Hoody <124743189+devhoody@users.noreply.github.com>
2023-04-27 22:41:47 +0900

    [Bronze III] Title: 최소, 최대, Time: 1368 ms, Memory: 105784 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,19 +1,25 @@
 import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+ 
  
 public class Main {
-	public static void main(String[] args) {
-    
-		Scanner sc = new Scanner(System.in);
-        
-		int N = sc.nextInt();
+ 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		
+		int index = 0;
 		int[] arr = new int[N];
-        
-		for (int i = 0; i < N; i++) {
-			arr[i] = sc.nextInt();
+		while(st.hasMoreTokens()) {
+			arr[index] = Integer.parseInt(st.nextToken());
+			index++;
 		}
-        
-		sc.close();
+		
 		Arrays.sort(arr);
 		System.out.print(arr[0] + " " + arr[N - 1]);
 	}
