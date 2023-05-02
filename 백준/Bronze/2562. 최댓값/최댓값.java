9b598c93a640a188ef830657b463557cbbb4972a
Hoody <124743189+devhoody@users.noreply.github.com>
2023-05-02 20:02:21 +0900

    [Bronze III] Title: 최댓값, Time: 124 ms, Memory: 14048 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -7,22 +7,16 @@ public class Main{
         
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        int[] arr = new int[9];
-        
-        for(int i=0; i<9; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
-        
-        int max = 0;
-        int count = 0;
+        int max =0;
         int index = 0;
         
-        for(int value : arr) {
-            count ++;
+        for(int i=0; i<9; i++) {
             
-            if(value > max) {
+            int value = Integer.parseInt(br.readLine());
+            
+            if(value>max) {
                 max = value;
-                index = count;
+                index = i+1;
             }
         }
         
