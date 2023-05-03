f8685995498b01fed89a07670798d14bec523c3a
Hoody <124743189+devhoody@users.noreply.github.com>
2023-05-03 17:52:43 +0900

    [Bronze V] Title: 윤년, Time: 224 ms, Memory: 17704 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -1,14 +1,17 @@
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String args[]) {
+public class Main{
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
         int a = sc.nextInt();
         
-        if(a % 4 ==0 && a%100!=0 || a%400 ==0)
+        sc.close();
+        
+        if(a%4 == 0 && a%100 !=0 || a%400 ==0)
             System.out.println(1);
-        else 
+        else
             System.out.println(0);
+                
     }
 }
