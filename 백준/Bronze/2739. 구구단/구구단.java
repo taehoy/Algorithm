ab53ffe52da0325ad51bb5ecbffb36f1c67ead6b
Hoody <124743189+devhoody@users.noreply.github.com>
2023-05-04 20:57:46 +0900

    [Bronze V] Title: 구구단, Time: 216 ms, Memory: 17720 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
 
 public class Main{
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        
+        int a = sc.nextInt();
         
         for(int i=1; i<10; i++) {
-            System.out.println(num+" * "+i + " = " +(num*i));
+            System.out.printf("%d" + " * " + "%d" + " = " + "%d\n", a, i, a*i);
         }
     }
 }
