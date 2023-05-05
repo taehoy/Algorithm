23341fd9ef1e6e26212711ac8517a9216d16cd4f
Hoody <124743189+devhoody@users.noreply.github.com>
2023-05-05 16:20:11 +0900

    [Bronze V] Title: 구구단, Time: 168 ms, Memory: 16200 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,13 +1,15 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner sc =new Scanner(System.in);
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        int a = sc.nextInt();
+        int N = Integer.parseInt(br.readLine());
         
         for(int i=1; i<10; i++) {
-            System.out.printf("%d" + " * " + "%d" + " = " + "%d\n", a, i, a*i);
+            System.out.println(N + " * " +i + " = " + N*i);
         }
     }
 }
