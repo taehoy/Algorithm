ded041864ba9225a8c6cd899f6eaeacd5aad9e57
Hoody <124743189+devhoody@users.noreply.github.com>
2023-05-05 16:44:54 +0900

    [Bronze V] Title: A+B - 7, Time: 168 ms, Memory: 16112 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -1,15 +1,24 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.io.InputStreamReader;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        int T = sc.nextInt();
+        int T = Integer.parseInt(br.readLine());
         
-        for(int i=0; i<T; i++){
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            System.out.println("Case #"+(i+1)+": "+(a+b));
+        StringTokenizer st;
+        
+        for(int i=0; i<T; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            
+            System.out.println("Case #" + (i+1) +": " + 
+                              (Integer.parseInt(st.nextToken()) 
+                              + Integer.parseInt(st.nextToken())));
         }
+        
+        br.close();
     }
 }
