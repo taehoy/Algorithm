357e3b783232a3d98177e465d83c460a6c297039
Hoody <124743189+devhoody@users.noreply.github.com>
2023-05-05 16:12:53 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 840 ms, Memory: 222016 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -1,27 +1,26 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 
 public class Main{
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
+    
         int T = Integer.parseInt(br.readLine());
         
         StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
         
-        for (int i=0; i<T; i++) {
-            st = new StringTokenizer(br.readLine()," ");
-            bw.write((Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()))+ "\n");
-        }   
+        for(int i=0; i<T; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            sb.append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())).append('\n');
+        }
+        
         br.close();
         
-        bw.flush();
-        bw.close();
+        System.out.println(sb);
+        
     }
 }
