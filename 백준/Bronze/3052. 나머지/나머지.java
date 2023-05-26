e385941b6487774052e049724fb30e9416721c44
Hoody <124743189+devhoody@users.noreply.github.com>
2023-05-26 15:52:36 +0900

    [Bronze II] Title: 나머지, Time: 208 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int[] arr = new int[10];
+		boolean bl; 
+		int cnt = 0; 
+
+		for(int i=0; i<arr.length; i++ ) {
+			arr[i] = sc.nextInt() % 42;
+		}
+		for(int i=0; i<arr.length; i++ ) {
+			bl = false; 
+			for(int j=i+1; j<arr.length; j++ ) {
+				if( arr[i] == arr[j] ) { 
+					bl = true; 
+					break; 
+				}
+			}
+			if( bl == false ) {
+				cnt ++; 
+			} 
+		}
+        sc.close();
+		System.out.println(cnt);
+	}
+}
