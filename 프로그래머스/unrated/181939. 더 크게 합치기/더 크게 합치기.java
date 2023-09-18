61bc3e7bba3364a5b3965edd4fc771a7abf304b3
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2023-09-18 14:10:59 +0900

    [unrated] Title: 더 크게 합치기, Time: 7.50 ms, Memory: 69.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,16 +1,6 @@
 class Solution {
     public int solution(int a, int b) {
-        String strA = String.valueOf(a);
-        String strB = String.valueOf(b);
         
-        int result1 = Integer.parseInt(strA + strB);
-        int result2 = Integer.parseInt(strB + strA);
-        
-        int answer = 0;
-        if(result1 >= result2) {
-        	answer = result1;
-        } else
-        	answer = result2;
-        return answer;
+        return Math.max(Integer.parseInt(a + "" + b), Integer.parseInt(b + "" + a));
     }
 }
