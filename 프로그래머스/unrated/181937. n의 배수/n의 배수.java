293d9c575dabd324d8cd0831b83a006a648b317e
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2023-12-04 09:11:29 +0900

    [level unrated] Title: n의 배수, Time: 0.02 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181937. n의 배수/n의 배수.java
+++ 프로그래머스/unrated/181937. n의 배수/n의 배수.java
@@ -1,8 +1,6 @@
 class Solution {
     public int solution(int num, int n) {
-        if(num % n == 0) {
-            return 1;
-        } else 
-            return 0;
+        int answer = num % n == 0 ? 1 : 0;
+        return answer;
     }
 }
