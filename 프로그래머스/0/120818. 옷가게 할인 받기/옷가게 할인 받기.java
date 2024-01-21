42454364c585eb9f540ff5210c850f49292684a3
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-01-21 18:21:33 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.02 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -1,12 +1,8 @@
 class Solution {
     public int solution(int price) {
-        if(price >= 100000 && price < 300000) {
-            return (int)((double)(price*0.95));
-        } else if(300000<= price && price < 500000) {
-            return (int)((double)(price * 0.9));
-        } else if(500000<= price) {
-            return (int)((double)(price * 0.8));
-        }
+        if(price >= 500000) return (int)(price*0.8);
+        if(price >= 300000) return (int)(price*0.9);
+        if(price >= 100000) return (int)(price*0.95);
         return price;
     }
 }
