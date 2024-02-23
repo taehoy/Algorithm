ecfa51c341445c4884853918f6a5b341b2c453d5
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-02-23 15:04:01 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.74 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int sum = (9-0+1) * (9+0) / 2; // 전체 합
+        int numbersSum = Arrays.stream(numbers).sum();
+        return sum - numbersSum;
+    }
+}
