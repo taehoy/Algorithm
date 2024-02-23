eff528371ac6d06fe90f5e103b84b39af1bc9d6f
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-02-23 14:47:33 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(long num) {
+        int count = 0;
+        if(num ==1){
+            count = 0;
+        }
+        
+        while(num != 1) {
+            // 입력된 수 짝수 -> 2 나누기
+            if(num % 2 == 0) {
+                num = num / 2;
+            } else {  // 홀수 -> 3곱하고 1더하기
+                num = num * 3 + 1;
+            }
+            
+            count ++;
+            if(count == 500){
+                count = -1;
+                break;
+            }
+        }
+        return count;
+    }
+}
