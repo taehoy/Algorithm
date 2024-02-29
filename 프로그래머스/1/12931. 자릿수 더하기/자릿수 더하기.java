8733636043b74a518242eb5198a4085ae375339a
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-02-29 10:32:28 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.11 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -1,16 +1,14 @@
-import java.util.*;
-
 public class Solution {
     public int solution(int n) {
-        int answer = 0;
-        
-        String string = String.valueOf(n);
-        String[] strings = string.split("");
-        
-        for(String s : strings) {
-            int i = Integer.parseInt(s);
-            answer += i;
+        // 각 자리수 구하기
+        int sum = 0;
+        String str = String.valueOf(n);
+        String[] arr = str.split("");
+        for(int i=0; i<arr.length; i++){
+            sum += Integer.parseInt(arr[i]); // 합하기
         }
-        return answer;
+        
+        return sum;
+        
     }
 }
