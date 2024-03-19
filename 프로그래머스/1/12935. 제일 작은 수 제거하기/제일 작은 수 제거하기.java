135f1c032d1e07a1c92300d40cda212113bd6c21
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-03-19 17:38:41 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.09 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] solution(int[] arr) {
+        if(arr.length == 1){
+            arr[0] = -1;
+            return arr;
+        }
+        
+        int[] answer = new int[arr.length-1];
+        
+        int min = arr[0];
+        for(int i=1; i < arr.length; i++){
+            if(min > arr[i]){
+                min = arr[i];
+            }
+        }
+        
+        int index = 0; // answer 인덱스 증가용
+        // 최소값을 뺀 나머지를 answer 배열에 넣기
+        for(int i = 0; i < arr.length; i++){
+            if(min == arr[i]){
+                continue;
+            }
+            answer[index++] = arr[i];
+        }
+        
+        return answer;
+    }
+}
