beac458d71624d50125e8870136e15d35e2700f2
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-03-26 18:34:40 +0900

    [level 1] Title: 약수의 합, Time: 0.06 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -1,13 +1,11 @@
 class Solution {
     public int solution(int n) {
-        // 약수구하기
-        int sum = 0;
+        int answer = 0;
         for(int i=1; i<=n; i++){
-            if(n % i == 0)
-                sum += i;
+            if(n % i == 0) {
+                answer += i;
+            }
         }
-        
-        return sum;
-        
+        return answer;
     }
 }
