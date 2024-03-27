0d587676e451848a97269b09e369490c3a9765f6
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-03-27 15:20:17 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.03 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] solution(int n, int m) {
+        int max = Math.max(n,m);
+        int min = Math.min(n,m);
+        int[] answer = {gcd(max, min), lcm(max, min)};
+        
+        return answer;
+    }
+    public int gcd(int a, int b){
+        if(b == 0) return a;
+        return gcd(b, a%b);
+    }
+    
+    public int lcm(int a, int b){
+        return a*b / gcd(a, b);
+    }
+}
