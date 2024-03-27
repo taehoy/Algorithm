d46cddc4edca8d334e35c2b8b8e42872d2b47290
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-03-27 14:07:20 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 1.91 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,25 +1,28 @@
 class Solution {
     public String solution(String s) {
         String answer = "";
+        // 2." "로 나누어준다.
         String[] strArr = s.split(" ");
-        
-        for(int i = 0; i<strArr.length; i++){
-            String now = strArr[i];
-            
-            if(strArr[i].length() ==0) {
+    
+        for(int i =0; i<strArr.length; i++){
+            if(strArr[i].length() == 0) {
                 answer += " ";
             } else {
-                answer += now.substring(0,1).toUpperCase();
-                answer += now.substring(1, now.length()).toLowerCase();
+                // 3 공백 포함 x -> 3.1첫 문자 대문자 변환 후 추가
+                answer += strArr[i].substring(0,1).toUpperCase();
+                // 3 공백 포함 x -> 3.2 나머지 소문자 추가
+                answer += strArr[i].substring(1,strArr[i].length()).toLowerCase();
+                // 3 공백 포함 x -> 3.3 공백 추가
                 answer += " ";
-            }    
+            }
         }
         
         if(s.substring(s.length()-1, s.length()).equals(" ")){
-    		return answer;
-    	}
+            return answer;
+        }
+        // 4 마지막 공백 제거
+        answer.substring(0,answer.length()-1);
         
-        return answer.substring(0, answer.length()-1);
-        
+        return answer.substring(0,answer.length()-1);
     }
 }
