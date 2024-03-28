16e0f438cd738cd8a2635f20cf0b791b4e883e56
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-03-28 09:58:16 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 1.93 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,16 +1,25 @@
 class Solution {
     public String solution(String s) {
         String answer = "";
-        // 전체를 소문자화하여 공백일 경우 대문자로 변환한다.
-        // 2. 배열만들기
-        String[] arr = s.toLowerCase().split("");
-        // 3. boolean값 flag을 이용하여 대문자화하기
-        boolean flag = true;
-     
-        for(String ss : arr){
-            answer += flag ? ss.toUpperCase() : ss;  // 3.1 flag이 true면 대문자, 아니면 그대로 넣기
-            flag = ss.equals(" ") ? true : false; // 3.2 공백이면 다음 값 대문자하기
+        
+        String[] arr = s.split(" ");
+        
+        for(int i=0; i<arr.length; i++){
+            // 공백일 경우 공백추가
+            if(arr[i].length() == 0){
+                answer += " ";
+            } else { // 공백이 아닐 경우 첫문자 대문자, 소문자, 공백추가
+                answer += arr[i].substring(0,1).toUpperCase();
+                answer += arr[i].substring(1,arr[i].length()).toLowerCase();
+                answer += " ";
+            }
         }
-        return answer;
+                    
+        //원래 문자열의 마지막이 공백일 경우 그대로 출력
+        if(s.substring(s.length()-1, s.length()).equals(" ")){
+            return answer;
+        }
+        //원래 문자열의 마지막이 공백이 아닐 경우, 공백 제거후 출력 
+        return answer.substring(0,answer.length()-1);
     }
 }
