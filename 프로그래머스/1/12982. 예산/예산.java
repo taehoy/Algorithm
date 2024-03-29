0e0022a1a14cede6c6fe85b2f7bdacf05346c2cb
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-03-29 10:23:38 +0900

    [level 1] Title: 예산, Time: 0.54 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -4,8 +4,10 @@ class Solution {
     public int solution(int[] d, int budget) {
         int answer = 0;
         int sum = 0;
+        // 오름차순 정렬
         Arrays.sort(d);
         
+        // 인덱스 0부터 더한다. sum > budget 이면 중단 
         for(int i=0; i<d.length; i++){
             sum += d[i];
             if(sum > budget){
