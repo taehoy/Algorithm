c09eac227ff309ff8f87a731424a0d0fc2fc1839
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-03-29 10:27:16 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 4.19 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,23 +1,14 @@
 class Solution {
     public String solution(String s) {
         String answer = "";
-        String[] strArr = s.split("");
+        String[] arr = s.split("");
+        int cnt = 0;
         
-        int idx = 0;
-        for(int i=0; i<strArr.length; i++){
-            if(strArr[i].equals(" ")){
-                idx = 0;
-            } else if(idx % 2 == 0){
-                strArr[i] = strArr[i].toUpperCase();
-                idx++;
-            } else if(idx % 2 == 1){
-                strArr[i] = strArr[i].toLowerCase();
-                idx++;
-            }
-            answer += strArr[i];
+        for(String ss : arr){
+            cnt = ss.contains(" ") ? 0 : cnt+1;
+            answer += cnt % 2 ==0 ? ss.toLowerCase() : ss.toUpperCase();
         }
         
         return answer;
-        
     }
 }
