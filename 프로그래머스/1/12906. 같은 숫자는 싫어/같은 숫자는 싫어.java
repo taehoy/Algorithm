e23e7fdd99edf06aa3a7425ce85a11428be9d11c
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-03-29 10:15:27 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 24.72 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,18 +1,19 @@
 import java.util.*;
 
 public class Solution {
-    public int[] solution(int[] arr) {
-        ArrayList<Integer> list = new ArrayList<>();
+    public int[] solution(int []arr) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
         list.add(arr[0]);
+        
         for(int i=1; i<arr.length; i++){
-            if(arr[i-1] == arr[i]){
-                continue;
-            } else {
+            if(arr[i-1] != arr[i]){
                 list.add(arr[i]);
             }
         }
+        
         int[] answer = new int[list.size()];
-        for(int i=0; i<list.size(); i++){
+        
+        for(int i=0; i<answer.length; i++){
             answer[i] = list.get(i);
         }
         
