6f4f953231be4889c1bfe82895be5c50409fcbda
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-03-30 14:57:19 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 2.45 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,25 +1,22 @@
 class Solution {
     public String solution(String s) {
         String answer = "";
-        
         String[] arr = s.split(" ");
         
         for(int i=0; i<arr.length; i++){
-            // 공백일 경우 공백추가
             if(arr[i].length() == 0){
                 answer += " ";
-            } else { // 공백이 아닐 경우 첫문자 대문자, 소문자, 공백추가
+            } else {
                 answer += arr[i].substring(0,1).toUpperCase();
-                answer += arr[i].substring(1,arr[i].length()).toLowerCase();
+                answer += arr[i].substring(1, arr[i].length()).toLowerCase();
                 answer += " ";
             }
         }
-                    
-        //원래 문자열의 마지막이 공백일 경우 그대로 출력
+        
         if(s.substring(s.length()-1, s.length()).equals(" ")){
             return answer;
         }
-        //원래 문자열의 마지막이 공백이 아닐 경우, 공백 제거후 출력 
-        return answer.substring(0,answer.length()-1);
+        
+        return answer.substring(0, answer.length()-1);
     }
 }
