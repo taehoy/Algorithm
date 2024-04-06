75026ef6fbe6c665b17f304475d1057c6eb8354b
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-04-06 22:56:56 +0900

    [level 1] Title: 삼총사, Time: 0.02 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -5,7 +5,7 @@ class Solution {
             for(int j=i+1; j<number.length-1; j++){
                 for(int k=j+1; k<number.length; k++){
                     if(number[i]+ number[j] + number[k] == 0){
-                        answer++;
+                          answer++;
                     }
                 }
             }
