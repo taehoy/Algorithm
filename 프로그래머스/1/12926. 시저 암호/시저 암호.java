cfa1dcccb2c7de3840b70c40ce949b780f2c0b51
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-04-08 20:48:13 +0900

    [level 1] Title: 시저 암호, Time: 34.27 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,25 +1,29 @@
 class Solution {
     public String solution(String s, int n) {
         String answer = "";
+        
         for(int i=0; i<s.length(); i++){
             if(s.charAt(i) == ' '){
                 answer += " ";
-            } else if('a' <= s.charAt(i) && s.charAt(i) <='z'){
-                int sumCh = s.charAt(i) +n;
-                if( 'z' < sumCh){
+            } else if('a' <= s.charAt(i) && s.charAt(i) <= 'z'){
+                int sumCh = s.charAt(i) + n;
+                if(sumCh > 'z'){
                     sumCh -= 26;
+                    answer += (char)sumCh;
+                } else {
+                    answer += (char)sumCh;    
                 }
-                char ch = (char)sumCh;
-                answer += ch;
             } else {
-                int sumCh = s.charAt(i) +n;
-                if('Z' < sumCh){
+                int sumCh = s.charAt(i) + n;
+                if(sumCh > 'Z'){
                     sumCh -= 26;
+                    answer += (char)sumCh;
+                } else {
+                    answer += (char)sumCh;
                 }
-                char ch = (char)sumCh;
-                answer += ch;
             }
         }
+        
         return answer;
     }
 }
