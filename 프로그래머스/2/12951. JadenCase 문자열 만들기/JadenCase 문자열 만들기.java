9dbc753f33c7df2c538fe0a02e3cb884b0ebbf00
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-04-10 18:33:18 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 1.36 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,10 +1,11 @@
 class Solution {
     public String solution(String s) {
         String answer = "";
-        String[] arr = s.toLowerCase().split("");
-        boolean flag = true;
         
-        for(String ss : arr){
+        String[] strArr = s.toLowerCase().split("");
+        boolean flag = true; // 첫문자는 무조건 대문자
+        
+        for(String ss : strArr){
             answer += flag ? ss.toUpperCase() : ss;
             flag = ss.equals(" ") ? true : false;
         }
