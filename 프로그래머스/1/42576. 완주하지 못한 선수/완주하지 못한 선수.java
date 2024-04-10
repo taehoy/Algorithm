f68d0dbcc15f29702153b9d60166ff3ae3d90c46
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-04-10 18:24:46 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 79.40 ms, Memory: 95.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,23 +2,22 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        
         HashMap<String, Integer> map = new HashMap<>();
         
-        for(int i=0; i<participant.length; i++){
-            map.put(participant[i], map.getOrDefault(participant[i],0) +1);
+        for(String player : participant){
+            map.put(player, map.getOrDefault(player, 0) +1);
         }
-
+        
+        // 완주자가 있으면 player key의 값 -1
         for(String player : completion){
             map.put(player, map.get(player)-1);
         }
         
-        String answer = "";
-        
-        for (String s : map.keySet()){
-            if(map.get(s) == 1){
-                answer = s;
-                break;
-            }
+        // key의 값이 1인 그사람이 바로 미완주자
+        for(String player : map.keySet()){
+            if(map.get(player) == 1) answer = player;
         }
         
         return answer;
