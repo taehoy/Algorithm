a339e9ddbfcb49f94b90781d3d8ca3fa0c1c5859
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-04-10 18:17:20 +0900

    [level 1] Title: 폰켓몬, Time: 3.29 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,20 +2,15 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        int pickNum = nums.length / 2;
-        
-        // 중복 제거
-        HashSet<Integer> numsSet = new HashSet<>();
+        HashSet<Integer> set = new HashSet<>();
         
         for(int num : nums){
-            numsSet.add(num);
+            set.add(num);
         }
         
-        // set의 크기가 pickNum보다 크거나 같으면 pickNum, 작으면 set의 크기 반환
-        if(numsSet.size() >= pickNum){
-            return pickNum;
-        } else {
-            return numsSet.size();
-        }
+        int pick = nums.length/2;
+        
+        if(set.size() >= pick) return pick;
+        else return set.size();
     }
 }
