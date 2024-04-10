57bb48ab7452f6d34c600a6f0b0067241e913104
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-04-11 07:28:49 +0900

    [level 1] Title: 콜라 문제, Time: 0.02 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -1,17 +1,15 @@
 class Solution {
     public int solution(int a, int b, int n) {
-        int answer = 0; // 돌려받은 개수 변수
-        
-        // n을 a로 나눈다.
-        while(n / a >= 1){
-            int getNum = (n / a) * b; // 받은 콜라 수 
-            
-            System.out.println("받은 콜라 수 : " + getNum);
-            System.out.println("가지고 있는 콜라 병 수 : " + (n/a) + (n%a));
-            answer += getNum; // 돌려받은 개수 변수에 추가
-            n = getNum + n % a;
+        int answer = 0;
+        return recursion(a, b, n, 0);
+    }
+    
+    public int recursion(int a, int b, int n, int result){
+        if(n < a){
+            return result;
         }
-        
-        return answer;
+        result += (n / a) * b;
+        n = ((n / a) *b) + (n % a);
+        return recursion(a, b, n, result);
     }
 }
