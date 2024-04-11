23c1a79d083043c2a42ff0ccb545a3e188cebe7b
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-04-11 17:28:38 +0900

    [Silver II] Title: DFS와 BFS, Time: 288 ms, Memory: 23828 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,80 +1,78 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-public class Main {
+public class Main{
+    static StringBuilder sb = new StringBuilder();
+    // visited
+    static boolean[] visited;
+    // 2차배열
+    static int[][] arr;
+    // node, line, start
+    static int node, line, start;
+    // que
+    static Queue<Integer> que = new LinkedList<>();
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-	static StringBuilder sb = new StringBuilder();
-	static boolean[] check;
-	static int[][] arr;
-	
-	static int node, line, start;
-	
-	static Queue<Integer> q = new LinkedList<>();
+        // 노드, 라인 수, 시작 값 받기
+        node = Integer.parseInt(st.nextToken());
+        line = Integer.parseInt(st.nextToken());
+        start = Integer.parseInt(st.nextToken());
 
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		node = Integer.parseInt(st.nextToken());
-		line = Integer.parseInt(st.nextToken());
-		start= Integer.parseInt(st.nextToken());
-		
-		arr = new int[node+1][node+1];
-		check = new boolean[node+1];
-		
-		for(int i = 0 ; i < line ; i ++) {
-			StringTokenizer str = new StringTokenizer(br.readLine());
-			
-			int a = Integer.parseInt(str.nextToken());
-			int b = Integer.parseInt(str.nextToken());
-			
-			arr[a][b] = arr[b][a] =  1;	
-		}
-			//sb.append("\n");
-			dfs(start);
-			sb.append("\n");
-			check = new boolean[node+1];
-			
-			bfs(start);
-			
-			System.out.println(sb);
-		
-		}
-	public static void dfs(int start) {
-		
-		check[start] = true;
-		sb.append(start + " ");
-		
-		for(int i = 0 ; i <= node ; i++) {
-			if(arr[start][i] == 1 && !check[i])
-				dfs(i);
-		}
-		
-	}
-	
-	public static void bfs(int start) {
-		q.add(start);
-		check[start] = true;
-		
-		while(!q.isEmpty()) {
-			
-			start = q.poll();
-			sb.append(start + " ");
-			
-			for(int i = 1 ; i <= node ; i++) {
-				if(arr[start][i] == 1 && !check[i]) {
-					q.add(i);
-					check[i] = true;
-				}
-			}
-		}
-		
-		
-	}
 
+        // 2차 배열 설정하기
+        arr = new int[node+1][node+1];
+
+        // dfs 체크용 visited 생성
+        visited = new boolean[node+1];
+
+        // 2차 배열 값 넣기 -> 연결되어있으면 1
+        for(int i=0; i<line; i++){
+            StringTokenizer str = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(str.nextToken());
+            int b = Integer.parseInt(str.nextToken());
+
+            arr[a][b] = arr[b][a] = 1;
+        }
+
+        // dfs
+        dfs(start);
+        sb.append("\n");
+        // visited 새로 초기화
+        visited = new boolean[node+1];
+        // bfs
+        bfs(start);
+
+        System.out.println(sb);
+    }
+    
+    static void bfs(int start) {
+        visited[start] = true;
+        que.add(start);
+
+        while(!que.isEmpty()){
+            start = que.poll();
+            sb.append(start+" ");
+
+            for(int i=1; i<=node; i++){
+                if(!visited[i] && arr[start][i] == 1){
+                    que.add(i);
+                    visited[i]= true;
+                }
+            }
+        }
+    }
+    
+    static void dfs(int start) {
+        visited[start] = true;
+        sb.append(start + " ");
+
+        for(int i=0; i<=node; i++){
+            if(!visited[i] && arr[start][i] == 1){
+                dfs(i);
+            }
+        }
+    }
 }
