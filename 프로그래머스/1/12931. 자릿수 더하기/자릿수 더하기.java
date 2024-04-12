75e1e943c6d820b350c077f2964706465b63e9d4
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-04-12 21:25:54 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -4,12 +4,11 @@ public class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        while(n >= 10){
+        while(n>=10){
             answer += n % 10;
             n = n / 10;
         }
         answer += n;
-        
         return answer;
     }
 }
