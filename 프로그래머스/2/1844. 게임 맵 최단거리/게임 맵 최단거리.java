f1622202635c5e88457232b99cdf4ec758dee6d0
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-04-12 21:01:09 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.95 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,12 +1,20 @@
 import java.util.*;
 
 class Solution {
-    static final int[] rowArr = {1, -1, 0, 0}, colArr = {0, 0, 1, -1};
+    // +, - 만들기
+    static int[] rowArr = {1,-1,0,0};
+    static int[] colArr = {0,0,1,-1};
+    // visited, arr[[][] 만들기
+    static boolean[][] visited;
+    static int[][] arr;
+    int rowLength, colLength;
     
-    static class Node{
-        final int row, col, move;
-        
-        public Node(int row, int col, int move){
+    static Queue<Node> que = new LinkedList<>();
+    
+    // 현재위치 node 만들기
+    public class Node{
+        int row, col, move;
+        Node(int row, int col, int move){
             this.row = row;
             this.col = col;
             this.move = move;
@@ -14,38 +22,38 @@ class Solution {
     }
     
     public int solution(int[][] maps) {
-        // visited 만들기
-        int rowLength = maps.length; // 행 길이
-        int colLength = maps[0].length; // 열 길이
+        // maps의 길이만큼 visited, arr 생성하기
+        rowLength = maps.length;
+        colLength = maps[0].length;
         
-        boolean[][] visited = new boolean[rowLength][colLength];
+        visited = new boolean[maps.length+1][colLength+1];
+        arr = new int[rowLength+1][colLength+1];
         
-        // 0일 경우 true
+        // visited값 설정하기
         for(int i=0; i<rowLength; i++){
-            for(int j=0; j<maps[i].length; j++) {
+            for(int j=0; j<colLength; j++){
                 if(maps[i][j] == 0) visited[i][j] = true;
             }
         }
         
-        // bfs만들기
-        Queue<Node> que = new LinkedList<>();
-        que.offer(new Node(0,0,1)); // 시작 위치 node 생성
-        visited[0][0] = true; // 시작 위치 true
+        // bfs구현하기
+        que.offer(new Node(0,0,1));
+        visited[0][0] = true;
         int min = Integer.MAX_VALUE;
         
-        // 시작위치에서 이동하기
         while(!que.isEmpty()){
             Node node = que.poll();
             
             for(int i=0; i<4; i++){
-                int row = node.row + rowArr[i], col = node.col + colArr[i];
-                if(rowLength <= row || 0> row || colLength <= col || 0 > col) continue;
-                if(visited[row][col] == true) continue;
+                int row = node.row + rowArr[i];
+                int col = node.col + colArr[i];
                 
+                if(row <0 || row>=rowLength || col < 0 || col >= colLength) continue;
+                if(visited[row][col] == true) continue;
                 visited[row][col] = true;
                 que.offer(new Node(row, col, node.move+1));
                 
-                // 이동할 수 있는 것중 이동거리 최소값 반환
+                // 최소값 반환
                 if(row == rowLength-1 && col == colLength-1){
                     min = Math.min(min, node.move+1);
                 }
