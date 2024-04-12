fc440b4facbfb6ed7c4e2c70e5d1b70aef71b8db
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-04-12 20:14:19 +0900

    [level 2] Title: 타겟 넘버, Time: 0.44 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,29 +1,28 @@
 class Solution {
-    int[] numbers;
-    int target;
-    int answer;
-    
-    void dfs(int index, int sum){
-        // 탈출 조건
-        if(index == numbers.length){
-            if(sum == target) answer++;
-            return;
-        }
-        
-        // 동작 구현
-        dfs(index+1, sum+numbers[index]); // 더하기
-        dfs(index+1, sum-numbers[index]); // 빼기
-    }
+    static int[] numbers;
+    static int target;
+    static int answer;
     
     public int solution(int[] numbers, int target) {
-        answer = 0;
         this.numbers = numbers;
         this.target = target;
+        answer = 0;
         
         dfs(0,0);
         
         return answer;
     }
     
+    public void dfs(int index, int result){
+        // 탈출조건
+        if(index == numbers.length){
+            if(result == target) answer++;
+            return;
+        }
+        
+        // 동작 구현
+        dfs(index+1, result+numbers[index]);
+        dfs(index+1, result-numbers[index]);
+    }
     
 }
