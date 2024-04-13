0fc54c6612a19111b59a350f0d41c7bbf4388ae8
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-04-13 22:09:04 +0900

    [level 1] Title: 하샤드 수, Time: 0.10 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -2,12 +2,11 @@ class Solution {
     public boolean solution(int x) {
         int sum =0;
         String[] str = String.valueOf(x).split("");
+        
         for(int i=0; i<str.length; i++){
             sum += Integer.parseInt(str[i]);
         }
-        if(x % sum == 0) {
-            return true;
-        }
+        if(x % sum == 0) return true;
         
         return false;
     }
