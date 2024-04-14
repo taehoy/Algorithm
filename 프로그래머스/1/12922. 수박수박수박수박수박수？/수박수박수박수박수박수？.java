b9751d884d63c0b8b97e463d10fbc1077d6a5a94
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-04-14 23:12:24 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 2.87 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,6 +1,7 @@
 class Solution {
     public String solution(int n) {
         String answer = "";
+        
         // i가 홀수면 +"수", 짝수면 +"박"
         for(int i=1; i<=n; i++){
             if(i % 2 == 1){
