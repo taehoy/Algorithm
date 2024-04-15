fd391bf0d36216f6de02ab6fc134787f3c75791c
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-04-15 11:17:58 +0900

    [level 1] Title: 콜라 문제, Time: 0.02 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -1,15 +1,22 @@
 class Solution {
+    static int a;
+    static int b;
+    static int n;
     public int solution(int a, int b, int n) {
-        int answer = 0;
-        return recursion(a, b, n, 0);
+        this.a = a;
+        this.b = b;
+        this.n = n;
+        
+        return dfs(n,0);
     }
     
-    public int recursion(int a, int b, int n, int result){
-        if(n < a){
-            return result;
-        }
-        result += (n / a) * b;
-        n = ((n / a) *b) + (n % a);
-        return recursion(a, b, n, result);
+    public int dfs(int n, int result){
+        // 탈출조건
+        if(n < a) return result;
+        
+        // 동작 구현
+        result += n/a * b;
+        n = (n/a *b) + (n%a);
+        return dfs(n, result);
     }
 }
