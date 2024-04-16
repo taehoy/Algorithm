916b129d5ab8cfc85b1807b2643832696c4abf1b
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-04-16 10:35:50 +0900

    [level 1] Title: 카드 뭉치, Time: 0.18 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -1,13 +1,36 @@
+import java.util.*;
+
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        int idx1 = 0;
-        int idx2 = 0;
+        String answer = "";
         
+        // 큐를 만든다.
+        Queue<String> gQueue = new LinkedList<>();
+        Queue<String> c1Queue = new LinkedList<>();
+        Queue<String> c2Queue = new LinkedList<>();
+        
+        // 큐에 값 넣자.
         for(String s : goal){
-            if(idx1 < cards1.length && cards1[idx1].equals(s)) idx1++;
-            else if(idx2 < cards2.length && cards2[idx2].equals(s)) idx2++;
+            gQueue.add(s);
+        }  
+        for(String s : cards1){
+            c1Queue.add(s);
+        }
+        for(String s : cards2){
+            c2Queue.add(s);
+        }
+        
+        // goal이 빌떄까지 loop
+        while(!gQueue.isEmpty()){
+            // goal에서 하나 꺼냄
+            String s = gQueue.poll();
+        
+            // 꺼낸 값이 두 큐에서 꺼낸 값과 일치한지 확인
+            if(s.equals(c1Queue.peek())) c1Queue.poll();
+            else if(s.equals(c2Queue.peek())) c2Queue.poll();
             else return "No";
         }
+        
         return "Yes";
     }
 }
