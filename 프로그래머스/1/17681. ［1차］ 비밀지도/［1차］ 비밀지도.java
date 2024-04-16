cc2eee31f18c1783afa6e535d3b7673677a5d71f
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-04-16 09:59:31 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.04 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -2,22 +2,20 @@ class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[arr1.length];
         
-        // arr1, arr2를 이진법으로 변환
         for(int i=0; i<arr1.length; i++){
-            answer[i] = Integer.toBinaryString(arr1[i] | arr2[i]);    
-            if(answer[i].length() != n) {
+            answer[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
+            if(answer[i].length() != n){
                 while(answer[i].length() != n){
                     answer[i] = "0" + answer[i];
                 }
             }
         }
         
-        // 1과 0을 각각 #과 " "치환
         for(int i=0; i<answer.length; i++){
-            answer[i] = answer[i].replaceAll("1", "#");
             answer[i] = answer[i].replace("0", " ");
+            answer[i] = answer[i].replace("1", "#");
         }
-
+        
         return answer;
     }
 }
