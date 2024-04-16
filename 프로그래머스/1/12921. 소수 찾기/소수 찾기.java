2e46b152fc6d19ab06c36a98d005970a3bcce38d
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-04-16 11:17:04 +0900

    [level 1] Title: 소수 찾기, Time: 12.44 ms, Memory: 52.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        boolean[] check = new boolean[n+1];
+        
+        for(int i=2; i<=n; i++){
+            if(check[i] == true) continue;
+            for(int j= i+i; j<=n; j+=i){
+                check[j] = true;
+            }
+        }
+        
+        for(int i=2; i<=n; i++){
+            if(!check[i]) answer++;
+        }
+        return answer;
+        
+    }
+}
