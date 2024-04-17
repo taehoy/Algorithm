98ee36655db319081751c5c457a281bbb045c51d
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-04-17 19:55:11 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,17 +1,18 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        int carpet = brown + yellow;
         
-        for(int i=3; i<=carpet; i++){
-            int row = i;
-            int col = carpet / row;
+        int sum = brown + yellow;
+        
+        for(int i=3; i<sum; i++){
+            int j = sum / i;
             
-            if(col < 3) continue;
-            
-            if(col >= row){
+            if(sum % i == 0 && j >= 3){
+                int col = Math.max(i, j);
+                int row = Math.min(i ,j);
                 int center = (col-2) * (row-2);
-                if(center == yellow){
+                
+                if(center == yellow) {
                     answer[0] = col;
                     answer[1] = row;
                     return answer;
