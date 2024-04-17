96fb94cb837ff801b69a6b08296b4dc34d43687c
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-04-17 19:46:14 +0900

    [level 1] Title: 소수 만들기, Time: 0.05 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -1,10 +1,7 @@
-import java.util.*;
-
 class Solution {
-    private boolean isPrime(int num){
-        for(int i=2; i <= Math.sqrt(num); i++){
+    public static boolean isPrime(int num){
+        for(int i=2; i<=Math.sqrt(num); i++){
             if(num % i == 0) return false;
-            
         }
         return true;
     }
@@ -15,10 +12,10 @@ class Solution {
         for(int i=0; i<nums.length; i++){
             for(int j=i+1; j<nums.length; j++){
                 for(int k=j+1; k<nums.length; k++){
-                    int sum = nums[i]+nums[j]+nums[k];
-                    
-                    answer += isPrime(sum) ? 1 : 0;
-                    
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if(isPrime(sum)){
+                        answer++;   
+                    }
                 }
             }
         }
