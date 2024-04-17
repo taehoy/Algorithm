f8b7eb079fa9f2563270c2ddf872fbcdaa8a76b1
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-04-17 19:41:08 +0900

    [level 1] Title: 소수 찾기, Time: 166.70 ms, Memory: 51.9 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/README.md
+++ 프로그래머스/1/12921. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 소수 찾기 - 12921 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12921?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12921) 
 
 ### 성능 요약
 
-메모리: 52.9 MB, 시간: 12.44 ms
+메모리: 51.9 MB, 시간: 166.70 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 16일 11:17:00
+2024년 04월 17일 19:41:03
 
 ### 문제 설명
 
--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -1,20 +1,17 @@
 class Solution {
+    // 소수 찾는 메서드
+    public static boolean isPrime(int num){
+        for(int i=2; i<=Math.sqrt(num); i++){
+            if(num % i == 0) return false;
+        }
+        return true;
+    }
+    
     public int solution(int n) {
         int answer = 0;
-        
-        boolean[] check = new boolean[n+1];
-        
         for(int i=2; i<=n; i++){
-            if(check[i] == true) continue;
-            for(int j= i+i; j<=n; j+=i){
-                check[j] = true;
-            }
-        }
-        
-        for(int i=2; i<=n; i++){
-            if(!check[i]) answer++;
+            if(isPrime(i)) answer ++;
         }
         return answer;
-        
     }
 }
