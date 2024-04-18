7da15cc592ab38c3fa69cf90923e03e4b895c0b1
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-04-19 00:27:38 +0900

    [Bronze II] Title: 문자열 반복, Time: 120 ms, Memory: 13984 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -5,25 +5,26 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        // 테스트케이스 수 받기
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer();
 
+        // 테스트케이스 수 받기
         int testCnt = Integer.parseInt(br.readLine());
 
         // 각 케이스 받기 , Stringtokenzier
         for(int i=0; i<testCnt; i++){
-            String answer = "";
             StringTokenizer st = new StringTokenizer(br.readLine());
             int num = Integer.parseInt(st.nextToken());
             String str = st.nextToken();
 
-            // 입력받은 문자열의 길이 * 반복수만큼 loop
-            for(int j=0; j<str.length() * num; j++){
-                // loop돌면서 각 단어 * 반복횟수를 sb에 추가한다.
-                answer += str.charAt(j/num) + "";
+            for(byte value : str.getBytes()){
+                for(int j=0; j<num; j++){
+                    sb.append((char) value);
+                }
             }
-            System.out.println(answer);
-
+            sb.append("\n");
         }
+
+        System.out.println(sb);
     }
 }
