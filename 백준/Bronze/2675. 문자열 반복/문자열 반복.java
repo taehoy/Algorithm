419b32f104ebb0312a06032886a54ee3aeaae22b
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-04-19 00:10:37 +0900

    [Bronze II] Title: 문자열 반복, Time: 152 ms, Memory: 16052 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -1,22 +1,29 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
- 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		
-		int T = in.nextInt();
-		for(int i = 0; i < T; i++) {
-	
-			int R = in.nextInt();
-			String S = in.next();	// nextLine() 을 쓰면 error!
-			
-			for(int j = 0; j < S.length(); j++) {           
-				for(int k = 0; k < R; k++) {	// R 만큼 반복 출력
-					System.out.print(S.charAt(j));
-				}
-			}
-            
-			System.out.println();
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        // 테스트케이스 수 받기
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testCnt = Integer.parseInt(br.readLine());
+
+        // 각 케이스 받기 , Stringtokenzier
+        for(int i=0; i<testCnt; i++){
+            String answer = "";
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int num = Integer.parseInt(st.nextToken());
+            String str = st.nextToken();
+
+            // 입력받은 문자열의 길이 * 반복수만큼 loop
+            for(int j=0; j<str.length() * num; j++){
+                // loop돌면서 각 단어 * 반복횟수를 sb에 추가한다.
+                answer += str.charAt(j/num) + "";
+            }
+            System.out.println(answer);
+
+        }
+    }
 }
