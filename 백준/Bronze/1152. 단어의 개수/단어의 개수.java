e5b7504fe3570f9f7c16de3ad2acdf5d4fecf75d
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-04-18 20:48:19 +0900

    [Bronze II] Title: 단어의 개수, Time: 236 ms, Memory: 18456 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -1,20 +1,14 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
- 
-public class Main {
- 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
- 
-		String S = in.nextLine();
-		in.close();
- 
-		// st 에 공백을 기준으로 나눈 토큰들을 st 에 저장한다
-		StringTokenizer st = new StringTokenizer(S," ");
-		
-		// countTokens() 는 토큰의 개수를 반환한다
-		System.out.println(st.countTokens());	
-		
-	}
- 
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        System.out.println(st.countTokens());
+    }
 }
