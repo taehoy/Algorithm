2e98f21530f6f11eac023fad259943c0cdbd103b
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-04-19 07:51:15 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 140 ms, Memory: 15912 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,29 +1,17 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        // 문자열 받기
+public class Main{
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer();
         String str = br.readLine();
-
-        // -1 배열 만들기 : 길이 26
-        int[] arr = new int[26];
-        for(int i=0; i<arr.length; i++){
-            arr[i] = -1;
+        
+        // a ~ z까지 순회
+        for(char c = 'a'; c <= 'z'; c++){
+            //indexOf이용하여 인덱스 위치 설정 
+            sb.append(str.indexOf(c) + " ");
         }
-
-        // 받은 문자열의 0번째부터 순회
-        for(int i=0; i<str.length(); i++){
-            int num = str.charAt(i) -'a'; // s[0] - 'a' = -1의 인덱스
-            if(arr[num] != -1) continue;
-            arr[num] = i; // 문자열인덱스 = 값
-        }
-
-        // -1배열 출력
-        for(int i : arr){
-            System.out.print(i + " ");
-        }
+        System.out.println(sb);
     }
 }
