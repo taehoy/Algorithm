ac281f40da35bb954813fc1fc8317271fddd5941
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-04-19 01:47:27 +0900

    [Bronze I] Title: 단어 공부, Time: 268 ms, Memory: 20104 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,27 +1,54 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.next().toUpperCase();
-
-        int[] count = new int[26];
-
-        for (int i = 0; i < str.length(); i++) {
-            int num = str.charAt(i) -'A' ;
-            count[num]++;
+    public static void main(String[] args) throws IOException {
+        // 문자열 입력받기
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        
+        // 값이 0 인 배열 만든다 -> 길이: 26, 문자 count용 배열
+        int[] arr = new int[26];
+        for(int i=0; i<26; i++){
+            arr[i] = 0;
         }
 
-        int max = 0;
-        char answer = '?';
-        for (int i = 0; i < count.length; i++) {
-            if(max < count[i]){
-                max = count[i];
-                answer = (char)(i+'A');
-            } else if (max == count[i]){
-                answer = '?';
+        // 문자열 순회한다.
+        for(int i=0; i<str.length(); i++){
+            // 해당 문자의 -'a'한 값 = 값이 0인 배열의 인덱스이므로 +1
+            if('a' <= str.charAt(i) && str.charAt(i) <= 'z'){
+                arr[str.charAt(i) - 'a']++; // 소문자일경우 -'a'
+            } else {
+                arr[str.charAt(i) - 'A']++; // 대문자일 경우 -'A'
             }
         }
-        System.out.println(answer);
+
+        // 기존 배열 복사 배열 만들기
+        int[] temp = new int[26];
+        for(int i=0; i<temp.length; i++){
+            temp[i] = arr[i];
+        }
+
+        // 정수 배열 오름차순 정렬
+        Arrays.sort(arr);
+
+        // 만약, 정수 배열의 인덱스[length-1] 과 [length-2]이 같다면 중복 -> ? 출력
+        if(arr[arr.length-1] == arr[arr.length-2]) System.out.println("?");
+        else {
+            int index = 0; // 출력할 인덱스
+
+            // 오름차순한 정렬의 arr[length-1]번째 값을 이용해서 기존 복사 배열에서 같은 값이 위치한 index 구하기
+            for(int i=0; i<temp.length; i++){
+                if(arr[arr.length-1] == temp[i]) {
+                    index = i;
+                }
+            }
+            // 중복되지 않으면 'A' + 인덱스 출력
+            char c = (char)('A' + index);
+            System.out.println(c);
+        }
+
     }
 }
