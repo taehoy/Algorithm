95638769616a3f7e97f68a9f9ff624c4dbfbf673
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-04-19 08:12:24 +0900

    [Silver IV] Title: 괄호, Time: 152 ms, Memory: 15008 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,34 +1,44 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
 
-public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        
-        int T = sc.nextInt();
-        
-        for(int i=0; i<T; i++){
-            System.out.println(solved(sc.next()));
-        }
-    }
-    
-    public static String solved(String s){
-        Stack<Character> stack = new Stack<>();
-
-        for(int i=0; i<s.length(); i++){
-            if(s.charAt(i) == '('){
-                stack.push('(');
+public class Main {
+    public static String solution(String s, BufferedReader br) throws IOException {
+        String answer = "";
+        // 문자열을 배열로 전황한다.
+        String[] arr = s.split("");
+        // 스택을 만든다.
+        Stack<String> stack = new Stack<>();
+        // 문자열 배열 순회한다.
+        for(int j=0; j<arr.length; j++){
+            // (면 push
+            if(arr[j].equals("(")){
+                stack.push("(");
             } else {
+                // )이고 비어있다면 "NO출력후 break
                 if(stack.isEmpty()){
-                    return "NO";                   
+                    answer = "NO";
+                    return answer;
+                } else {
+                    // )이고 안비어있으면 pop
+                    stack.pop();
                 }
-                stack.pop();
             }
         }
 
-        if(stack.isEmpty()){
-            return "YES";
-        } else {
-            return "NO";
+        if(!stack.isEmpty()) return "NO";
+
+        return "YES";
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        // T테스트케이스 순회
+        for(int i=0; i<T; i++){
+            System.out.println(solution(br.readLine(), br));
         }
     }
 }
