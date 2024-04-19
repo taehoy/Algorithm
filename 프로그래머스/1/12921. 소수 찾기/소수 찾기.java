156d428d53b5f18c647f4829af21707d54f1633e
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-04-20 08:47:51 +0900

    [level 1] Title: 소수 찾기, Time: 223.60 ms, Memory: 52 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -1,17 +1,21 @@
 class Solution {
-    // 소수 찾는 메서드
-    public static boolean isPrime(int num){
-        for(int i=2; i<=Math.sqrt(num); i++){
-            if(num % i == 0) return false;
+    public boolean isPrime(int n){
+        for(int i=2; i<=Math.sqrt(n); i++){
+            if(n % i == 0) return false;
         }
         return true;
     }
     
+    
     public int solution(int n) {
         int answer = 0;
+        
         for(int i=2; i<=n; i++){
-            if(isPrime(i)) answer ++;
+            if(isPrime(i)){
+               answer++;
+            }    
         }
+        
         return answer;
     }
 }
