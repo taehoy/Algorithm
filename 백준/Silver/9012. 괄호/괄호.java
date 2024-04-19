586d19d963216bdb965c91df46f848c4f0cf3f43
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-04-20 08:33:43 +0900

    [Silver IV] Title: 괄호, Time: 128 ms, Memory: 14384 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -4,41 +4,34 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class Main {
-    public static String solution(String s, BufferedReader br) throws IOException {
-        String answer = "";
-        // 문자열을 배열로 전황한다.
-        String[] arr = s.split("");
-        // 스택을 만든다.
-        Stack<String> stack = new Stack<>();
-        // 문자열 배열 순회한다.
-        for(int j=0; j<arr.length; j++){
-            // (면 push
-            if(arr[j].equals("(")){
-                stack.push("(");
-            } else {
-                // )이고 비어있다면 "NO출력후 break
-                if(stack.isEmpty()){
-                    answer = "NO";
-                    return answer;
-                } else {
-                    // )이고 안비어있으면 pop
-                    stack.pop();
-                }
-            }
-        }
-
-        if(!stack.isEmpty()) return "NO";
-
-        return "YES";
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
 
-        // T테스트케이스 순회
         for(int i=0; i<T; i++){
             System.out.println(solution(br.readLine(), br));
         }
+
+    }
+
+    private static String solution(String str, BufferedReader br) throws IOException {
+        Stack<Character> stack = new Stack<>();
+        for(int i=0; i<str.length(); i++){
+            if(str.charAt(i) == '('){
+                stack.push('(');
+            } else {
+                if(stack.isEmpty()){
+                    return "NO";
+                }
+                stack.pop();
+            }
+        }
+
+        if(!stack.isEmpty()){
+            return "NO";
+        }
+
+        return "YES";
+
     }
 }
