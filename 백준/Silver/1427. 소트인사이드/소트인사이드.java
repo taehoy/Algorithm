32a716bd3ef3c503ae27f431de6b333ee5dae684
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-04-20 10:50:46 +0900

    [Silver V] Title: 소트인사이드, Time: 124 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,28 +1,24 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
+import java.io.InputStream;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
-        String[] arr = s.split("");
 
-        // byte 배열의 각 요소 크기 비교 -> 내림차순 정렬
-        Arrays.sort(arr, new Comparator<String>(){
-            public int compare(String s1, String s2){
-                return Integer.parseInt(s2) - Integer.parseInt(s1);
-            }
-        });
+        InputStream in = System.in;
 
-        StringBuilder sb = new StringBuilder();
+        int[] counting = new int[10];
 
-        for(String ss : arr){
-            sb.append(ss);
+        int c;
+        while((c = in.read()) != '\n'){
+            counting[c - '0']++;
         }
 
-        System.out.println(sb);
+        for(int i = counting.length-1; i>=0; i--){
+            while(counting[i]-- > 0){
+                System.out.print(i);
+            }
+        }
+
+
     }
 }
