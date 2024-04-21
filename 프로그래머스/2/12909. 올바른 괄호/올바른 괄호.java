9a8cebc27835ca34ca2d2e9896017b7d5a6b2ebf
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-04-21 23:18:11 +0900

    [level 2] Title: 올바른 괄호, Time: 18.98 ms, Memory: 52.9 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -3,6 +3,7 @@ import java.util.*;
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
+        
         Stack<String> stack = new Stack<>();
         for(int i=0; i<s.length(); i++){
             if(s.charAt(i) == '('){
