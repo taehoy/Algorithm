1547162ddd6716a39af9b322abab9fa721234899
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-04-22 04:04:38 +0900

    [Silver V] Title: 단어 정렬, Time: 372 ms, Memory: 27896 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,44 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
+import java.util.SimpleTimeZone;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        
-        int T = Integer.parseInt(br.readLine());
 
-        // 문자열 배열 만든다.
-        String[] strArr = new String[T];
+        // 반복 횟수 받기
+        String s = br.readLine();
+        int size = Integer.parseInt(s);
 
-        // 문자열 배열에 입력 값 넣기
-        for(int i=0; i<T; i++){
-            strArr[i] = br.readLine();
+        // 문자열 배열 생성 및 배열에 값 넣기
+        String[] arr = new String[size];
+
+        for(int i=0; i<size; i++){
+            arr[i] = br.readLine();
         }
 
-        Arrays.sort(strArr, new Comparator<String>(){
+        // 문자열 길이 및 사전순 정렬 시전
+        Arrays.sort(arr, new Comparator<String>(){
             public int compare(String s1, String s2){
-                // 길이가 같으면 사전순 정렬
+                // 같으면 사전순
                 if(s1.length() == s2.length()){
                     return s1.compareTo(s2);
                 } else {
-                    // 길이 다르면 오름차순 정렬
+                    // 문자열 길이별 순서
                     return s1.length() - s2.length();
                 }
             }
         });
 
-        sb.append(strArr[0] + "\n");
+        // 중복 빼고 출력하기
+        StringBuilder sb = new StringBuilder();
 
-        // 중복되면 패스
-        for(int i=1; i< strArr.length; i++){
-            if(!strArr[i].equals(strArr[i-1])){
-                sb.append(strArr[i]+"\n");
+        sb.append(arr[0] + "\n");
+
+        for(int i=1; i<arr.length; i++){
+            if(!arr[i].equals(arr[i-1])){
+                sb.append(arr[i] + "\n");
             }
         }
 
