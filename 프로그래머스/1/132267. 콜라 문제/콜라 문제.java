eda65f30ef5cb36972cc68cefa99f31b0f7033e5
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-04-25 08:54:00 +0900

    [level 1] Title: 콜라 문제, Time: 0.02 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -1,22 +1,18 @@
 class Solution {
-    static int a;
-    static int b;
-    static int n;
     public int solution(int a, int b, int n) {
-        this.a = a;
-        this.b = b;
-        this.n = n;
+        // 받는 콜라 병수 int 변수
+        int answer = 0;
         
-        return dfs(n,0);
-    }
-    
-    public int dfs(int n, int result){
-        // 탈출조건
-        if(n < a) return result;
-        
-        // 동작 구현
-        result += n/a * b;
-        n = (n/a *b) + (n%a);
-        return dfs(n, result);
+        // 가지고 있는 병수(n)이 줘야하는 병수(a) 보다 작으면 끝. a>n
+        while(a <= n){
+            // 받는 병수 (n / a) * b
+            int getedCoke = (n / a) * b;
+            
+            n = (n%a) + getedCoke;
+            
+            // 남은 병수(n) -> (n % a) + 받는 병수
+            answer += getedCoke;
+        }
+        return answer;
     }
 }
