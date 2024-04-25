7e52c20f11fc4f8d71e81b70e58066d9dc6ee6e7
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-04-25 09:23:33 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.30 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -2,17 +2,24 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int k, int[] score) {
+        // 최저점수 배열 생성 길이 : score.length
         int[] answer = new int[score.length];
         
+        // 프리오리티 큐 생성(순서o)
         PriorityQueue<Integer> queue = new PriorityQueue<>();
         
+
+        // 큐에 값 넣기
         for(int i=0; i<score.length; i++){
+            // 큐에 값 넣기
             queue.add(score[i]);
             
-            if(queue.size()>k){
+            // 큐의 크기가 k보다 크면 빼기
+            if(queue.size() > k){
                 queue.poll();
             }
             
+            // 큐의 맨 앞의 값을 배열에 넣는다.
             answer[i] = queue.peek();
         }
         
