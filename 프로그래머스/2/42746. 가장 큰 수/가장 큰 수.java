d9ed1f2b7f0f7d149ef696a0f27402dd85e152f6
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-04-26 08:59:48 +0900

    [level 2] Title: 가장 큰 수, Time: 1.78 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -3,17 +3,16 @@ import java.util.*;
 class Solution {
     public String solution(int[] numbers) {
         String answer = "";
+        String[] arr = new String[numbers.length];
         StringBuilder sb = new StringBuilder();
         
-        String[] arr = new String[numbers.length];
-    
-        for(int i=0; i<arr.length; i++){
+        for(int i=0; i<numbers.length; i++){
             arr[i] = String.valueOf(numbers[i]);
         }
         
+        // 더한 값 비교 
         Arrays.sort(arr, new Comparator<String>(){
             public int compare(String s1, String s2){
-                
                 return (s2 + s1).compareTo(s1 + s2);
             }
         });
@@ -24,7 +23,7 @@ class Solution {
             sb.append(s);
         }
         
-        answer = sb.toString();
+        answer += sb;
         
         return answer;
     }
