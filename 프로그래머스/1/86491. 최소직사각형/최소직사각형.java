b2389fb6d0d9a42a48e3097e80a0963185baae18
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-04-30 08:50:45 +0900

    [level 1] Title: 최소직사각형, Time: 2.22 ms, Memory: 92.1 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,13 +1,17 @@
 class Solution {
     public int solution(int[][] sizes) {
-        int v_max = 0;
-        int h_max = 0;
+        int v_max = 0; // 가로
+        int h_max = 0; // 세로
         
-        for(int[] card : sizes){
-            v_max = Math.max(v_max, Math.max(card[0], card[1]));
-            h_max = Math.max(h_max, Math.min(card[0], card[1]));
+        // 명함 수만큼 반복
+        for(int i=0; i<sizes.length; i++){
+            // 길이가 긴 것을 가로로 세팅 후 최대값 구하기
+            v_max = Math.max(v_max,Math.max(sizes[i][0], sizes[i][1]));
+            // 길이가 짧은 것 세로
+            h_max = Math.max(h_max,Math.min(sizes[i][0], sizes[i][1]));
         }
         
+        // 명함 크기 반환(가로 * 세로)
         return v_max * h_max;
     }
 }
