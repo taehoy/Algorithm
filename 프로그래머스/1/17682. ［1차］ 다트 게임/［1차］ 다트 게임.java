eb8eb899d99718945de08ccc85ba8cc9b6c021ef
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-05-01 10:05:36 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.05 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -1,36 +1,30 @@
 class Solution {
     public int solution(String dartResult) {
-        int answer = 0;
-        int[] scores = new int[3]; // 점수배열
-        int idx = -1; // 점수 배열 인덱스
-        
         char[] arr = dartResult.toCharArray();
+        int[] scores = new int[3];
+        int idx = -1;
         
         for(int i=0; i<arr.length; i++){
-            // 숫자일 경우
             if('0' <= arr[i] && arr[i] <= '9'){
+                idx++;
                 if(arr[i+1] == '0'){
-                    idx++; // 숫자면 idx ++
-                    scores[idx] = 10; 
-                    i++; // 10이므로 다음 idx jump
-                    continue;
-                } else {
-                    idx++;
-                    scores[idx] = Integer.parseInt(String.valueOf(arr[i]));
+                    scores[idx] = 10;
+                    i++;
                     continue;
                 }
+                scores[idx] = Integer.parseInt(String.valueOf(arr[i]));
+                continue;
             }
-            // 문자일 경우
-            switch(arr[i]) {
-                case 'D' :
+            switch(arr[i]){
+                case 'D' : 
                     scores[idx] = (int)Math.pow(scores[idx], 2);
                     break;
-                case 'T' :
+                case 'T' : 
                     scores[idx] = (int)Math.pow(scores[idx], 3);
                     break;
-                case '*' :
+                case '*' : 
                     scores[idx] *= 2;
-                    if(idx-1 >=0) scores[idx-1] *= 2;
+                    if(idx-1>=0) scores[idx-1] *= 2;
                     break;
                 case '#' :
                     scores[idx] *= -1;
