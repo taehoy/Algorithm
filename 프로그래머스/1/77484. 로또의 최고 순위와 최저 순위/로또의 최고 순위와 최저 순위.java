6b6e1b483e39100c5a61424ed1e94a79ab5d73ee
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-05-01 21:19:21 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.04 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,73 +1,22 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
-        int[] answer = new int[2];
-        
-        // 자료 준비
+        // 최대개수, 최소개수
         int max = 0;
         int min = 0;
         
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int num : win_nums){
-            map.put(num, 1);
-        }
-        
-        // 최고개수, 최저 개수 구하기
-        for(int i=0; i<lottos.length; i++){
-            if(lottos[i] == 0){
+        for(int i : lottos){
+            if(i == 0){
                 max++;
             } else {
-                if(map.containsKey(lottos[i])){
-                    max++;
-                    min++;
+                for(int j : win_nums){
+                    if(i == j){
+                        max++;
+                        min++;
+                    }
                 }
             }
         }
         
-        // 점수에 따른 순위 매기기
-        switch(max){
-            case 6 : 
-                answer[0] = 1;
-                break;
-            case 5 : 
-                answer[0] = 2;
-                break;
-            case 4 : 
-                answer[0] = 3;
-                break;
-            case 3 : 
-                answer[0] = 4;
-                break;
-            case 2 : 
-                answer[0] = 5;
-                break;
-            default : 
-                answer[0] = 6;
-                break;
-        }
-        
-        switch(min){
-            case 6 : 
-                answer[1] = 1;
-                break;
-            case 5 : 
-                answer[1] = 2;
-                break;
-            case 4 : 
-                answer[1] = 3;
-                break;
-            case 3 : 
-                answer[1] = 4;
-                break;
-            case 2 : 
-                answer[1] = 5;
-                break;
-            default : 
-                answer[1] = 6;
-                break;
-        }
-    
-        return answer;
+        return new int[] {Math.min(7-max, 6), Math.min(7-min, 6)};
     }
 }
