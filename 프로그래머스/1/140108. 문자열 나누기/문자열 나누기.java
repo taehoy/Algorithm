bf7f575a5f3008635c7c5c82afb143ddd2b220c6
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-05-01 21:25:55 +0900

    [level 1] Title: 문자열 나누기, Time: 0.48 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -1,38 +1,28 @@
 class Solution {
     public int solution(String s) {
-        int answer = 0; // 분리 개수
-        // 단어 일치 수
-        int sameWordCnt = 1;
-        int diffWordCnt = 0;
+        int answer = 0;
+        char ch = s.charAt(0);
         
-        // 문자열 -> 문자 배열
-        char[] arr = s.toCharArray();       
-        char word = arr[0];
+        int count = 0;
         
-        if(arr.length == 1) return 1;
-        
-        for(int i=1; i<arr.length; i++){
-            if(word == arr[i]) sameWordCnt++;
-            else diffWordCnt++;
-            
-            // 단어 수가 일치하면 나눈다.
-            if(sameWordCnt == diffWordCnt) {
-                answer++; // 나눈 횟수 +1
-                
-                if(i+1 <= arr.length-1){
-                    word = arr[i+1]; 
-                    continue;
-                }
-                
-                if(i == arr.length-1) break;
-
+        for(char c : s.toCharArray()){
+            if(count == 0){
+                ch = c;
             }
-            
-            if(i == arr.length-1){
+            if(ch == c){
+                count++;
+            } else {
+                count--;
+            }
+            if(count == 0){
                 answer++;
             }
-            
         }
+        
+        if(count >0){
+            answer++;
+        }
+
         return answer;
     }
 }
