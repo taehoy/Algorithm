0f12ff50ef195fe0d03609f534e565ec7e69588c
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-05-02 19:19:32 +0900

    [level 1] Title: 체육복, Time: 0.05 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,35 +2,33 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
+        // 도난, 여분 Set 만들기
+        Set<Integer> lostSet = new HashSet<>();
+        Set<Integer> reserveSet = new HashSet<>();
         
-        int answer = n-lost.length;
+        // 도난 Set 값 넣기
+        for(int i : reserve){
+            reserveSet.add(i);
+        }
         
-        Arrays.sort(lost);
-        Arrays.sort(reserve);
-        
-        // 여분 있는 사람이 도난 당한 경우
-        for(int i=0; i<lost.length; i++){
-            for(int j=0; j<reserve.length; j++){
-                if(lost[i] == reserve[j]){
-                    answer++;
-                    lost[i] = -1;
-                    reserve[j] = -1;
-                    break;
-                }
+        // 여분이 있는 사람 중 도난 당한 사람 처리
+        for(int i : lost){
+            if(reserveSet.contains(i)){
+                reserveSet.remove(i);
+            } else{
+                lostSet.add(i);
             }
         }
         
-        // 도난 당한 사람의 앞뒤 번호 처리
-        for(int i=0; i<lost.length; i++){
-            for(int j=0; j<reserve.length; j++){
-                if(lost[i]-1 == reserve[j] || lost[i]+1 == reserve[j]){
-                    answer++;
-                    reserve[j] = -1;
-                    break;
-                }
+        // 여분 있는 사람이 도난 당한 사람 돕는 경우
+        for(Integer i : reserveSet){
+            if(lostSet.contains(i-1)){
+                lostSet.remove(i-1);
+            } else if(lostSet.contains(i+1)){
+                lostSet.remove(i+1);
             }
         }
         
-        return answer;
+        return n-lostSet.size();
     }
 }
