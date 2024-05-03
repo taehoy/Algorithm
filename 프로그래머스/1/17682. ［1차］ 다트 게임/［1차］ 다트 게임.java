531e131b418c09aae1de8cb4506dbbe857c75f06
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-05-03 09:15:39 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.07 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -1,34 +1,36 @@
 class Solution {
     public int solution(String dartResult) {
-        char[] arr = dartResult.toCharArray();
+        
         int[] scores = new int[3];
         int idx = -1;
+        char[] arr = dartResult.toCharArray();
         
         for(int i=0; i<arr.length; i++){
-            if('0' <= arr[i] && arr[i] <= '9'){
+            // 숫자인 경우
+            if('0'<=arr[i] && arr[i] <= '9'){
                 idx++;
+                // 10인 경우
                 if(arr[i+1] == '0'){
                     scores[idx] = 10;
                     i++;
                     continue;
                 }
+                // 0~9인 경우
                 scores[idx] = Integer.parseInt(String.valueOf(arr[i]));
                 continue;
             }
-            switch(arr[i]){
-                case 'D' : 
-                    scores[idx] = (int)Math.pow(scores[idx], 2);
-                    break;
-                case 'T' : 
-                    scores[idx] = (int)Math.pow(scores[idx], 3);
-                    break;
-                case '*' : 
-                    scores[idx] *= 2;
-                    if(idx-1>=0) scores[idx-1] *= 2;
-                    break;
-                case '#' :
-                    scores[idx] *= -1;
-                    break;
+            
+            // 문자인 경우
+            if(arr[i] == 'D'){
+                scores[idx] = (int)Math.pow(scores[idx], 2);
+            } else if(arr[i] == 'T'){
+                scores[idx] = (int)Math.pow(scores[idx], 3);
+            } else if(arr[i] == '*'){
+                scores[idx] *= 2;
+                // 첫번째 판이면 전 경기 없으므로 조건처리
+                if(idx -1 >= 0) scores[idx-1] *= 2;
+            } else if(arr[i] == '#'){
+                scores[idx] *= -1;
             }
         }
         
