2be60e61f23a5beb029174e47ede274722450331
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-05-03 21:30:10 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.04 ms, Memory: 70.8 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -3,37 +3,33 @@ class Solution {
         String answer = "";
         StringBuilder sb = new StringBuilder();
         
+        // X와 Y의 숫자 배열
         int[] arrX = new int[10];
         int[] arrY = new int[10];
         
+        // X와 Y의 숫자 비교
+        // X의 숫자 배열 값 대입
         for(int i=0; i<X.length(); i++){
-            arrX[X.charAt(i)-'0']++;
+            arrX[X.charAt(i) - '0']++;
         }
         
+        // Y의 숫자 배열 값 대입
         for(int i=0; i<Y.length(); i++){
-            arrY[Y.charAt(i)-'0']++;
-        }
-        
-        for(int i : arrX){
-            System.out.print(i + " | ");
-        }
-        System.out.println();
-        for(int i : arrY){
-            System.out.print(i + " | ");
+            arrY[Y.charAt(i) - '0']++;
         }
         
         for(int i=9; i>=0; i--){
-            while(arrX[i] >= 1 && arrY[i] >= 1){
+            while(arrX[i]>=1 && arrY[i]>= 1){
                 sb.append(String.valueOf(i));
                 arrX[i]--;
                 arrY[i]--;
             }
         }
         
-        // 짝꿍 없는 경우
+        // 짝꿍이 없으면 -1반환
         if(sb.length() == 0) return "-1";
         
-        // 짝꿍이 0밖에 없는 경우
+        // 짝꿍이 0으로만 구성되어있으면 0반환
         if(sb.toString().charAt(0) == '0') return "0";
         
         return sb.toString();
