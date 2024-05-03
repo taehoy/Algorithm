230f5505710e19797452c3c7f4c905ae15a503b0
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-05-03 10:39:15 +0900

    [level 2] Title: 숫자의 표현, Time: 1.52 ms, Memory: 52 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,19 +1,23 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
+        int sum = 0;
         
         for(int i=1; i<=n; i++){
-            int sum =0;
             for(int j=i; j<=n; j++){
-                sum +=j;
+                sum += j;
+                
                 if(sum == n){
                     answer++;
+                    sum = 0;
                     break;
                 } else if(sum > n){
+                    sum = 0;
                     break;
                 }
             }
         }
+        
         return answer;
     }
 }
