acf3ab7b2334551d1474ddbc2621991b41046d13
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-05-04 10:35:56 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.04 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,22 +1,32 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
-        // 점수 카운트
+        int[] answer = {};
+        
         int max = 0;
         int min = 0;
         
-        for(int i : lottos){
-            if(i == 0 ){
+        // Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
+        
+        // 맵에 당첨번호 넣기
+        for(int i : win_nums){
+            list.add(i);
+        }
+        
+        // 당첨번호와 매칭
+        for(int i = 0; i<lottos.length; i++){
+            if(lottos[i] == 0){
                 max++;
             } else {
-                for(int j : win_nums){
-                    if(i == j) {
-                        max++;
-                        min++;
-                    }
+                if(list.contains(lottos[i])){
+                    max++;
+                    min++;
                 }
             }
         }
         
-        return new int[]{Math.min(7-max, 6), Math.min(7-min, 6)};
+        return new int[] {Math.min(7-max, 6), Math.min(7-min, 6)};
     }
 }
