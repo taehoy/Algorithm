9c4d314d3c61ecc3166ff35ff355ffa111f75cdf
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-05-06 15:00:08 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.28 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        
+        // 바구니 만들기
+        Stack<Integer> stack = new Stack<>();
+        
+        // 인형 잡기
+        for(int i=0; i<moves.length; i++){
+            int pick = 0; // 집은 인형 번호
+            
+            // 인형상자에서 인형 집기
+            for(int j=0; j<board.length; j++){
+                if(board[j][moves[i]-1] != 0){
+                    pick = board[j][moves[i]-1];
+                    board[j][moves[i]-1] = 0;
+                    break;
+                }
+            }
+            
+            // 크레인의 열에 아무것도 없으면 건너뜀
+            if(pick == 0) continue;
+            
+            // 바구니와 비교
+            if(stack.isEmpty()){
+                stack.push(pick);
+            } else {
+                if(stack.peek() == pick){
+                    stack.pop();
+                    answer+=2;
+                    continue;
+                }
+                stack.push(pick);
+            }
+        }
+        
+        return answer;
+    }
+}
