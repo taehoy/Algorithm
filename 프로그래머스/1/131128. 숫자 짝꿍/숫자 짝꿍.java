372d15cbdc2c368181149b3cecebd0d35cf6c5d1
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-05-06 12:29:09 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.05 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -2,31 +2,26 @@ class Solution {
     public String solution(String X, String Y) {
         StringBuilder sb = new StringBuilder();
         
-        // 배열 만들기
         int[] arrX = new int[10];
         int[] arrY = new int[10];
         
-        // 각 숫자 배열에 값 넣기
-        for(int i = 0; i<X.length(); i++){
+        for(int i=0; i<X.length(); i++){
             arrX[X.charAt(i)-'0']++;
         }
-        for(int i = 0; i<Y.length(); i++){
+        
+        for(int i=0; i<Y.length(); i++){
             arrY[Y.charAt(i)-'0']++;
         }
         
-        // 함께 있는 수 구하기
         for(int i=9; i>=0; i--){
-            while(arrX[i] >= 1 && arrY[i] >= 1){
+            while(arrX[i] >=1 && arrY[i] >= 1){
                 sb.append(String.valueOf(i));
                 arrX[i]--;
                 arrY[i]--;
             }
         }
         
-        // 없으면 -1
         if(sb.length() == 0) return "-1";
-        
-        // 0이면 0
         if(sb.toString().charAt(0) == '0') return "0";
         
         return sb.toString();
