be2b83c33bbbe90f9db8d34735522ee6aed5c309
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-05-06 13:14:38 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.88 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -6,20 +6,19 @@ class Solution {
         
         for(int i=0; i<numbers.length-1; i++){
             for(int j=i+1; j<numbers.length; j++){
-                if(!list.contains(numbers[i] + numbers[j])){
-                    list.add(numbers[i] + numbers[j]);    
+                if(!list.contains(numbers[i]+ numbers[j])){
+                    list.add(numbers[i] + numbers[j]);
                 }
             }
         }
+        
+        Collections.sort(list);
         
         int[] answer = new int[list.size()];
         
         for(int i=0; i<answer.length; i++){
             answer[i] = list.get(i);
         }
-        
-        Arrays.sort(answer);
-        
         return answer;
     }
 }
