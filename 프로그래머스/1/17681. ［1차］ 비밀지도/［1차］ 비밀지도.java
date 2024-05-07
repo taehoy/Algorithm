5909ff31bf91b0994eae607b13606ebc59f552fa
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-05-07 10:07:53 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.06 ms, Memory: 70.3 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,19 +1,17 @@
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = new String[arr1.length];
+        String[] answer = new String[n];
         
+        // 두 지도 원소 비교
         for(int i=0; i<arr1.length; i++){
             answer[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
-            if(answer[i].length() != n){
-                while(answer[i].length() != n){
-                    answer[i] = "0" + answer[i];
-                }
+            
+            while(answer[i].length() != n){
+                answer[i] = "0" + answer[i];
             }
-        }
-        
-        for(int i=0; i<answer.length; i++){
-            answer[i] = answer[i].replace("0", " ");
-            answer[i] = answer[i].replace("1", "#");
+            
+            answer[i] = answer[i].replace("1","#");
+            answer[i] = answer[i].replace("0"," ");
         }
         
         return answer;
