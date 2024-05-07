42d24c14b7973b926db8b630cf8a694292be13a0
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-05-07 12:54:35 +0900

    [Silver V] Title: 셀프 넘버, Time: 144 ms, Memory: 14484 KB -BaekjoonHub

---
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
@@ -0,0 +1,41 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // 체크배열 생성
+        boolean[] check = new boolean[10001];
+
+        // 1~10000까지 순회
+        for(int i=1; i<10001; i++){
+            int n = d(i);
+
+            // n이 10000 초과는 의미 없음
+            if(n < 10001){
+                check[n] = true;
+            }
+        }
+
+        // 생성자 x 숫자 출력
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=1; i<10001; i++){
+            if(check[i] == false){
+                sb.append(i + "\n");
+            }
+        }
+
+        System.out.println(sb);
+
+    }
+
+    public static int d(int n){
+        int sum = n;
+
+        while(n != 0){
+            sum += n % 10;
+            n = n/10;
+        }
+
+        return sum;
+    }
+}
