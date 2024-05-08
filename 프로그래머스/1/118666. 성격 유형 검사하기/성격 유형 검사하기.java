65ccbc5aa7e0f2fe5f35cd4decfcb1a01887ecad
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-05-08 15:54:58 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.82 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -1,31 +1,7 @@
 import java.util.*;
 
 class Solution {
-    public String solution(String[] survey, int[] choices) {
-        StringBuilder sb = new StringBuilder();
-        
-        Map<Character, Integer> map = new HashMap<>();
-        
-        // 성격 유형 결과지 만들기
-        map.put('R',0);
-        map.put('T',0);
-        map.put('C',0);
-        map.put('F',0);
-        map.put('J',0);
-        map.put('M',0);
-        map.put('A',0);
-        map.put('N',0);
-        
-        // 점수 매기기
-        for(int i=0; i<survey.length; i++){
-            if(1 <= choices[i] && choices[i] <= 3){
-                map.put(survey[i].charAt(0), map.getOrDefault(survey[i].charAt(0), 0) + 4-choices[i]);
-            } else if(5 <= choices[i] && choices[i] <= 7){
-                map.put(survey[i].charAt(1), map.getOrDefault(survey[i].charAt(1), 0) + (choices[i]-4));
-            } 
-        }
-        
-        // 최종 점수 비교
+    public void makeResult(Map<Character, Integer>  map, StringBuilder sb){
         if(map.get('R') >= map.get('T')) sb.append("R");
         else if(map.get('R') < map.get('T')) sb.append("T");
 
@@ -37,7 +13,47 @@ class Solution {
 
         if(map.get('A') >= map.get('N')) sb.append("A");
         else sb.append("N");
+    }
+    
+    public void writeScore(Map<Character, Integer> map, String[] survey, int[] choices){
         
+        for(int i=0; i<survey.length; i++){
+            char c1 = survey[i].charAt(0);
+            char c2 = survey[i].charAt(1);
+            
+            if(1 <= choices[i] && choices[i] <= 3){
+                map.put(c1, map.getOrDefault(c1, 0) + 4-choices[i]);
+            } else if(5 <= choices[i] && choices[i] <= 7){
+                map.put(c2, map.getOrDefault(c2, 0) + choices[i]-4);
+            } 
+        }
+    }
+    
+    public Map makeScoreBorad(){
+        Map<Character, Integer> map = new HashMap<>();
+        
+        char [][] type = {{'R', 'T'}, {'C', 'F'}, {'J', 'M'}, {'A', 'N'}};
+        
+        // 성격 유형 결과지 만들기
+        for (char[] t : type) {
+            map.put(t[0], 0);
+            map.put(t[1], 0);
+        }
+        
+        return map;
+    }
+    
+    public String solution(String[] survey, int[] choices) {
+        Map<Character, Integer> map = makeScoreBorad();
+        
+        // 점수 매기기
+        writeScore(map, survey, choices);
+        
+        
+        // 최종 점수 비교
+        StringBuilder sb = new StringBuilder();
+        
+        makeResult(map, sb);
         
         return sb.toString();
     }
