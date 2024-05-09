ceb20307869473719313447016a9ad5731136d92
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-05-09 21:00:10 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.71 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/README.md
+++ 프로그래머스/1/72410. 신규 아이디 추천/README.md
@@ -1,10 +1,10 @@
 # [level 1] 신규 아이디 추천 - 72410 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72410#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72410) 
 
 ### 성능 요약
 
-메모리: 73.1 MB, 시간: 0.60 ms
+메모리: 75.9 MB, 시간: 0.71 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 09일 14:17:14
+2024년 05월 09일 21:00:07
 
 ### 문제 설명
 
--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,101 +1,99 @@
-
-
 class Solution {
     public String solution(String new_id) {
-        
-        String s = new KakaoID(new_id)
-                        .replcaeLowerCase()
+        String kakaoID = new KakaoID(new_id)
+                        .toLowerCase()
                         .filter()
                         .toSingleDot()
-                        .firstEndDotDel()
-                        .noBlank()
-                        .noLongerThan16()
-                        .toLongerTwo()
+                        .removeFirstOrEndDot()      
+                        .checkLength()
+                        .tolessThan16()
+                        .checkLength2()
                         .getResult();
         
-        return s;
+        System.out.println(kakaoID);
+        
+        return kakaoID;
     }
     
-    private static class KakaoID {
+    private static class KakaoID{
         private String s;
-
-        public KakaoID(String s){
-            this.s = s;
+        
+        public KakaoID(String new_id){
+            this.s = new_id;
         }
-
-        private KakaoID replcaeLowerCase(){
+        
+        private KakaoID toLowerCase(){
             s = s.toLowerCase();
             return this;
         }
-
+        
         private KakaoID filter(){
-            char[] sArr = s.toCharArray();
+            char[] arr = s.toCharArray();
             StringBuilder sb = new StringBuilder();
-            for(char c : sArr){
-                if(('a'<=c && c <='z') || ('0' <= c && c <= '9') || c =='_' || c== '-' || c == '.' ){
+            
+            for(char c : arr){
+                if(('a' <= c && c <= 'z') || ('0' <= c && c <='9') || c == '-' || c == '_' || c == '.'){
                     sb.append(c);
                 }
             }
             this.s = sb.toString();
-
+            
             return this;
         }
-
+        
         private KakaoID toSingleDot(){
             s = s.replace("..", ".");
             while(s.contains("..")){
                 s = s.replace("..", ".");
             }
-
+            
             return this;
         }
-
-        private KakaoID firstEndDotDel(){
-
+        
+        private KakaoID removeFirstOrEndDot(){
             if(s.length() > 0){
-                if(s.charAt(0) == '.'){
-                    s = s.substring(1, s.length());
-                }
+                if(s.charAt(0) == '.') s = s.substring(1,s.length());
             }
             if(s.length() > 0){
-                if(s.charAt(s.length()-1) == '.'){
-                    s = s.substring(0, s.length()-1);
-                }
-            }
-            return this;
-        }
-
-        private KakaoID noBlank(){
-            s = s.equals("") ? "a" : s;
-            return this;
-        }
-
-        private KakaoID noLongerThan16(){
-            if(s.length() >= 16){
-                s = s.substring(0,15);
-            }
-            if(s.charAt(s.length()-1) == '.'){
-                s = s.substring(0,14);
+                if(s.charAt(s.length()-1) == '.') s = s.substring(0,s.length()-1);
             }
             
             return this;
         }
-
-        private KakaoID toLongerTwo(){
+        
+        private KakaoID checkLength(){
+            s = s.equals("") ? "a" : s;
+            return this;
+        }
+        
+        private KakaoID tolessThan16(){
+            if(s.length() >= 16){
+                s = s.substring(0,15);
+            }
+            if(s.charAt(s.length()-1)== '.') s = s.substring(0,14);
+            
+            return this;
+        }
+        
+        private KakaoID checkLength2(){
             StringBuilder sb = new StringBuilder(s);
+            
             if(sb.length() <= 2){
                 while(sb.length() != 3){
                     sb.append(s.charAt(s.length()-1));
                 }
             }
-
             s = sb.toString();
+            
             return this;
         }
-
+        
+        
         private String getResult(){
             return s;
         }
-
+        
+        
+        
     }
 }
