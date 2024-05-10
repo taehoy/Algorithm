fa3933dd9ec8319e0ad2edd93c722ff5f628a40f
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-05-10 10:02:25 +0900

    [level 1] Title: 모의고사, Time: 0.55 ms, Memory: 68.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -3,37 +3,28 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] answers) {
         
-        // 수포자 배열
         int[] arr1 = {1, 2, 3, 4, 5};
         int[] arr2 = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] arr3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         
-        // 수포자 점수 계산
         int[] scores = new int[3];
         
         for(int i=0; i<answers.length; i++){
-            if(answers[i] == arr1[i%5]) scores[0]++;
-            if(answers[i] == arr2[i%8]) scores[1]++;
-            if(answers[i] == arr3[i%10]) scores[2]++;
+            if(answers[i] == arr1[i % 5]) scores[0]++;
+            if(answers[i] == arr2[i % 8]) scores[1]++;
+            if(answers[i] == arr3[i % 10]) scores[2]++;
         }
         
-        // 최대 점수
-        int max = Math.max(scores[0], Math.max(scores[1], scores[2]));
-        
-        // 수포자-점수 Map 만들기
-        Map<Integer, Integer> map = new HashMap<>();
-        
-        for(int i=0; i<3; i++){
-            map.put(i+1, scores[i]);
-        }
+        int maxScore = Math.max(scores[0] , Math.max(scores[1], scores[2]));
         
         List<Integer> list = new ArrayList<>();
         
-        for(int i=0; i<map.size(); i++){
-            if(map.get(i+1) == max) list.add(i+1);
+        for(int i=0; i<scores.length; i++){
+            if(scores[i] == maxScore) {
+                list.add(i+1);
+            }
         }
         
-        // 리스트 -> 배열
         int[] answer = new int[list.size()];
         
         for(int i=0; i<answer.length; i++){
