ab5abbc77ec5e4089fbbff4b4307c60613bf7a2c
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-05-11 14:03:29 +0900

    [level 1] Title: 둘만의 암호, Time: 1.13 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/README.md
+++ 프로그래머스/1/155652. 둘만의 암호/README.md
@@ -1,10 +1,10 @@
 # [level 1] 둘만의 암호 - 155652 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155652) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155652#) 
 
 ### 성능 요약
 
-메모리: 76.7 MB, 시간: 0.84 ms
+메모리: 72.4 MB, 시간: 1.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 11일 13:52:33
+2024년 05월 11일 14:03:27
 
 ### 문제 설명
 
--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -4,41 +4,20 @@ class Solution {
     public String solution(String s, String skip, int index) {
         StringBuilder sb = new StringBuilder();
         
-        // skip 리스트 만들기
-        char[] arr = skip.toCharArray();
-        List<Character> list = new ArrayList<>();
-        for(char c : arr){
-            list.add(c);
-        }
-
-        // 알파벳 변환
-        
-        for(int i=0; i<s.length(); i++){
-            int count = 0; // 현재 변환횟수
-            char c = s.charAt(i); // 현재 알파벳
+        for(char c : s.toCharArray()){
             char currChar = c;
+            int count = 0;
             
-            // 정해진 횟수만큼 이동 시작
             while(count != index){
-                // 한번 이동
-                currChar = (char)(currChar + 1);
-                
-                // z를 넘을 경우 처리
-                if(currChar > 'z'){
-                    currChar = (char)(currChar - 26);
+                currChar = currChar== 'z' ? 'a' : (char)(currChar +1);
+
+                if(!skip.contains(String.valueOf(currChar))){
+                    count++;
                 }
-                
-                // // skip과 건너뛰기 유무 처리
-                if(list.contains(currChar)){
-                    continue;
-                }
-                
-                // skip에 없으면 알파벳 한칸 이동
-                count++;
             }
             
             sb.append(currChar);
-        } // 변환 끝
+        }
         
         return sb.toString();
     }
