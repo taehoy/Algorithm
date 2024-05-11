08be147d2f92ed163afe8fc2fde74283d40d45f8
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-05-11 13:05:45 +0900

    [level 1] Title: 공원 산책, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.java
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.java
@@ -1,59 +1,60 @@
 class Solution {
     public int[] solution(String[] park, String[] routes) {
         int[] answer = new int[2];
-        int x = -1; // 실제 좌표 x
-        int y = -1; // 실제 좌표 y
-        int height = park.length;
-        int weight = park[0].length();
+        int x = -1; // 시작 지점 및 최종위치
+        int y = -1; // 시작 지점 및 최종위치
+        int height = park.length; // 공원 세로
+        int weight = park[0].length(); // 공원 가로
         
-        // 시작 위치 지정
+        //시작 지점 찾기
         for(int i=0; i<park.length; i++){
             if(park[i].indexOf('S') != -1){
                 x = park[i].indexOf('S');
                 y = i;
+                break;
             }
         }
         
-        // 이동 시작
         
+        // 로봇 이동
         for(int i=0; i<routes.length; i++){
-            String[] arr = routes[i].split(" ");
-            String vector = arr[0]; // 방향
-            int distance = Integer.parseInt(arr[1]); // 이동거리
-            int currX = x;
-            int currY = y;
-                
+            String[] temp = routes[i].split(" ");
+            String vector = temp[0]; // 방향
+            int distance = Integer.parseInt(temp[1]);
+            int currX = x; // 현재 위치 x
+            int currY = y; // 현재 위치 y
+            
             if(vector.equals("E")){
                 boolean flag = true;
-
-                for(int j=0; j<distance; j++){
+                for(int j=1; j<=distance; j++){
                     currX++;
-                
+                    
+                    // 공원 넘어가는가
                     if(currX >= weight){
                         flag = false;
                         break;
                     } else if(park[currY].charAt(currX) == 'X'){
                         flag = false;
                         break;
-                    }    
+                    }
                 }
-                
+
                 if(flag){
                     x = currX;
                 }
             } else if(vector.equals("W")){
                 boolean flag = true;
-
-                for(int j=0; j<distance; j++){
+                for(int j=1; j<=distance; j++){
                     currX--;
-                
-                    if(currX <0){
+                    
+                    // 공원 넘어가는가
+                    if(currX < 0){
                         flag = false;
                         break;
-                    } else if(park[currY].charAt(currX) == 'X'){
+                    }else if(park[currY].charAt(currX) == 'X'){
                         flag = false;
                         break;
-                    }    
+                    }
                 }
                 
                 if(flag){
@@ -61,16 +62,17 @@ class Solution {
                 }
             } else if(vector.equals("S")){
                 boolean flag = true;
-                for(int j=0; j<distance; j++){
+                for(int j=1; j<=distance; j++){
                     currY++;
-                
+                    
+                    // 공원 넘어가는가
                     if(currY >= height){
                         flag = false;
                         break;
                     } else if(park[currY].charAt(currX) == 'X'){
                         flag = false;
                         break;
-                    }    
+                    }
                 }
                 
                 if(flag){
@@ -78,23 +80,24 @@ class Solution {
                 }
             } else if(vector.equals("N")){
                 boolean flag = true;
-                for(int j=0; j<distance; j++){
+                for(int j=1; j<=distance; j++){
                     currY--;
-                
+                    
+                    // 공원 넘어가는가
                     if(currY < 0){
                         flag = false;
                         break;
                     } else if(park[currY].charAt(currX) == 'X'){
                         flag = false;
                         break;
-                    }    
+                    }
                 }
                 
                 if(flag){
                     y = currY;
                 }
-            } 
-        } // 이동 끝
+            }
+        }
         
         answer[0] = y;
         answer[1] = x;
