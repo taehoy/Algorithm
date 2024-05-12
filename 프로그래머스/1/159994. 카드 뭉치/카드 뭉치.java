173e2d418704f9a6cf493579d002b40799b5ae89
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-05-12 13:35:55 +0900

    [level 1] Title: 카드 뭉치, Time: 0.14 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -4,32 +4,30 @@ class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
         String answer = "";
         
-        // 큐를 만든다.
-        Queue<String> gQueue = new LinkedList<>();
-        Queue<String> c1Queue = new LinkedList<>();
-        Queue<String> c2Queue = new LinkedList<>();
+        // 큐 생성
+        Queue<String> que1 = new LinkedList<>();
+        Queue<String> que2 = new LinkedList<>();
+        Queue<String> que3 = new LinkedList<>();
         
-        // 큐에 값 넣자.
-        for(String s : goal){
-            gQueue.add(s);
-        }  
-        for(String s : cards1){
-            c1Queue.add(s);
-        }
-        for(String s : cards2){
-            c2Queue.add(s);
-        }
+        // 큐에 추가
+        for(String s : cards1) que1.offer(s);
+        for(String s : cards2) que2.offer(s);
+        for(String s : goal) que3.offer(s);
         
-        // goal이 빌떄까지 loop
-        while(!gQueue.isEmpty()){
-            // goal에서 하나 꺼냄
-            String s = gQueue.poll();
-        
-            // 꺼낸 값이 두 큐에서 꺼낸 값과 일치한지 확인
-            if(s.equals(c1Queue.peek())) c1Queue.poll();
-            else if(s.equals(c2Queue.peek())) c2Queue.poll();
-            else return "No";
-        }
+        // 목표 단어 생성 여부 처리
+        for(int i=0; i<goal.length; i++){
+            String s1 = que1.peek();
+            String s2 = que2.peek();
+            String goalS = que3.peek();
+            
+            if((s1 != null) && s1.equals(goalS)) {
+                que3.poll();
+                que1.poll();
+            } else if((s2 != null) && s2.equals(goalS)){
+                que3.poll();
+                que2.poll();
+            } else return "No";
+        }     
         
         return "Yes";
     }
