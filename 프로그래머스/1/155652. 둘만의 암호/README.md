541764cc96c8b8b4bd18e44f3758e04fa49beec4
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-05-13 10:21:08 +0900

    [level 1] Title: 둘만의 암호, Time: 0.85 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/README.md
+++ 프로그래머스/1/155652. 둘만의 암호/README.md
@@ -1,10 +1,10 @@
 # [level 1] 둘만의 암호 - 155652 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155652#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155652) 
 
 ### 성능 요약
 
-메모리: 72.4 MB, 시간: 1.13 ms
+메모리: 73.7 MB, 시간: 0.85 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 11일 14:03:27
+2024년 05월 13일 10:21:06
 
 ### 문제 설명
 
--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -1,22 +1,19 @@
-import java.util.*;
-
 class Solution {
     public String solution(String s, String skip, int index) {
         StringBuilder sb = new StringBuilder();
         
         for(char c : s.toCharArray()){
-            char currChar = c;
             int count = 0;
             
             while(count != index){
-                currChar = currChar== 'z' ? 'a' : (char)(currChar +1);
-
-                if(!skip.contains(String.valueOf(currChar))){
+                c = (c == 'z') ? 'a' : (char)(c + 1);
+                
+                if(!skip.contains(String.valueOf(c))){
                     count++;
                 }
             }
             
-            sb.append(currChar);
+            sb.append(c);
         }
         
         return sb.toString();
