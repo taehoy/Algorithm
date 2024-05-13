e6f55fa37410442c95e285f83a689fb27484cf7f
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-05-13 09:32:44 +0900

    [level 1] Title: 2016년, Time: 0.02 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -1,6 +1,17 @@
 class Solution {
     public String solution(int a, int b) {
-        String answer = "";
-        return answer;
+        int day = 0;
+        
+        int[] monthDays = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] week = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
+        
+        // 일 구하기
+        for(int i=0; i<a; i++){
+            day += monthDays[i];
+        }
+        
+        day += (b-1);
+        
+        return week[day % 7];
     }
 }
