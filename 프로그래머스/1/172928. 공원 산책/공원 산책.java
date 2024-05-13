46d2f3b93e639dccba8e88d23b42a9b0c587e825
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-05-13 10:06:05 +0900

    [level 1] Title: 공원 산책, Time: 0.14 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.java
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.java
@@ -1,89 +1,37 @@
 class Solution {
     public int[] solution(String[] park, String[] routes) {
         int[] answer = new int[2];
-        int x = -1; // 시작 지점 및 최종위치
-        int y = -1; // 시작 지점 및 최종위치
-        int height = park.length; // 공원 세로
-        int weight = park[0].length(); // 공원 가로
         
-        //시작 지점 찾기
+        int x = -1; // 시작위치 및 로봇위치
+        int y = -1; // 시작위치 및 로봇위치
+        
+        int weight = park[0].length(); // 가로
+        int height = park.length; // 세로
+        
+        // 시작 위치 지정
         for(int i=0; i<park.length; i++){
             if(park[i].indexOf('S') != -1){
                 x = park[i].indexOf('S');
                 y = i;
-                break;
             }
         }
         
-        
         // 로봇 이동
         for(int i=0; i<routes.length; i++){
-            String[] temp = routes[i].split(" ");
-            String vector = temp[0]; // 방향
-            int distance = Integer.parseInt(temp[1]);
-            int currX = x; // 현재 위치 x
-            int currY = y; // 현재 위치 y
+            int currX = x;
+            int currY = y;
+            String[] strArr = routes[i].split(" ");
             
-            if(vector.equals("E")){
-                boolean flag = true;
-                for(int j=1; j<=distance; j++){
-                    currX++;
-                    
-                    // 공원 넘어가는가
-                    if(currX >= weight){
-                        flag = false;
-                        break;
-                    } else if(park[currY].charAt(currX) == 'X'){
-                        flag = false;
-                        break;
-                    }
-                }
-
-                if(flag){
-                    x = currX;
-                }
-            } else if(vector.equals("W")){
-                boolean flag = true;
-                for(int j=1; j<=distance; j++){
-                    currX--;
-                    
-                    // 공원 넘어가는가
-                    if(currX < 0){
-                        flag = false;
-                        break;
-                    }else if(park[currY].charAt(currX) == 'X'){
-                        flag = false;
-                        break;
-                    }
-                }
+            String vector = strArr[0];
+            int distance = Integer.parseInt(strArr[1]);
+            
+            if(vector.equals("N")){
                 
-                if(flag){
-                    x = currX;
-                }
-            } else if(vector.equals("S")){
                 boolean flag = true;
-                for(int j=1; j<=distance; j++){
-                    currY++;
-                    
-                    // 공원 넘어가는가
-                    if(currY >= height){
-                        flag = false;
-                        break;
-                    } else if(park[currY].charAt(currX) == 'X'){
-                        flag = false;
-                        break;
-                    }
-                }
                 
-                if(flag){
-                    y = currY;
-                }
-            } else if(vector.equals("N")){
-                boolean flag = true;
-                for(int j=1; j<=distance; j++){
+                for(int j=0; j<distance; j++){
                     currY--;
                     
-                    // 공원 넘어가는가
                     if(currY < 0){
                         flag = false;
                         break;
@@ -93,10 +41,58 @@ class Solution {
                     }
                 }
                 
-                if(flag){
-                    y = currY;
+                if(flag) y = currY;
+                
+            } else if(vector.equals("S")){
+                boolean flag = true;
+                
+                for(int j=0; j<distance; j++){
+                    currY++;
+                    
+                    if(currY >= height){
+                        flag = false;
+                        break;
+                    } else if(park[currY].charAt(currX) == 'X'){
+                        flag = false;
+                        break;
+                    }
                 }
+                
+                if(flag) y = currY;
+            } else if(vector.equals("W")){
+               boolean flag = true;
+                
+                for(int j=0; j<distance; j++){
+                    currX--;
+                    
+                    if(currX < 0){
+                        flag = false;
+                        break;
+                    } else if(park[currY].charAt(currX) == 'X'){
+                        flag = false;
+                        break;
+                    }
+                }
+                
+                if(flag) x = currX;
+            } if(vector.equals("E")){
+                boolean flag = true;
+                
+                for(int j=0; j<distance; j++){
+                    currX++;
+                    
+                    if(currX >= weight){
+                        flag = false;
+                        break;
+                    } else if(park[currY].charAt(currX) == 'X'){
+                        flag = false;
+                        break;
+                    }
+                }
+                
+                if(flag) x = currX;
             }
+            
         }
         
         answer[0] = y;
