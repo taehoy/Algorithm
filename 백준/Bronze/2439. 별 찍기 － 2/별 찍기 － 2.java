f86e1e507abcafa322484a4aaecc468efcce0d63
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-05-14 13:14:42 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 128 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -1,21 +1,24 @@
-import java.util.Scanner;
- 
+import java.io.*;
+
 public class Main {
- 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
- 
-		int N = in.nextInt();
-		in.close();
- 
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= N - i; j++) {
-				System.out.print(" ");
-			}
-			for (int k = 0; k < i; k++) {
-				System.out.print("*");
-			}
-			System.out.println();
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<N; i++){
+            for(int j=1; j<=N-i-1; j++){
+                bw.write(' ');
+            }
+            for(int j=0; j<=i; j++){
+                bw.write('*');
+            }
+            bw.newLine();
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
 }
