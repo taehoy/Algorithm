e305cb95f4bedabf8de1219dab6c74a7bdfe0396
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-05-14 10:36:22 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 120 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -1,4 +1,8 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -9,11 +13,11 @@ public class Main {
 
         br.close();
 
-        for(int i=1; i<=N; i++){
-            for(int j=1; j<=i; j++){
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= i; j++) {
                 bw.write('*');
             }
-            bw.write('\n');
+            bw.newLine();
         }
 
         bw.flush();
