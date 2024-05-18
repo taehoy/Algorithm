495ed6249a50f98862c11be7838760aef3c556e7
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-05-18 15:13:48 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 80 ms, Memory: 11588 KB -BaekjoonHub

--- 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -1,24 +1,25 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                sb.append(" ");
-            }
-            for (int j = n; j > i; j--) {
-                sb.append("*");
-            }
-            sb.append("\n");
+        for(int i=0; i<n; i++){
+            sb.append("*");
         }
 
-        System.out.print(sb);
+        for(int i=0; i<n; i++){
+            bw.write(sb.toString());
+            bw.newLine();
+            sb.replace(i, i+1, " ");
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
     }
 }
