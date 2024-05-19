15fd9b08f709d4957a296ef445a343ec474e10a7
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-05-20 08:36:48 +0900

    [level 1] Title: 대충 만든 자판, Time: 2.02 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
@@ -4,38 +4,33 @@ class Solution {
     public int[] solution(String[] keymap, String[] targets) {
         int[] answer = new int[targets.length];
         
-        Map<Character, Integer> map = new HashMap<>();
+        Map<Character, Integer> keypad = new HashMap<>();
         
         for(int i=0; i<keymap.length; i++){
             for(int j=0; j<keymap[i].length(); j++){
                 char c = keymap[i].charAt(j);
-                if(map.containsKey(c)){
-                    int idx = map.get(c);
-                    map.put(c, Math.min(idx, j+1));
-                } else {
-                    map.put(c, j+1);
+                
+                if(keypad.containsKey(c)){
+                    int cnt = keypad.get(c);
+                    keypad.put(c, Math.min(cnt, j+1));
+                } else{
+                    keypad.put(c, j+1);
                 }
             }
         }
         
-        // 타겟과 비교
         for(int i=0; i<targets.length; i++){
-            boolean flag = true;
-            int count = 0;
-            
             for(int j=0; j<targets[i].length(); j++){
                 char c = targets[i].charAt(j);
-                if(map.containsKey(c)){
-                    count += map.get(c);
+                
+                if(keypad.containsKey(c)){
+                    answer[i] += keypad.get(c);
                 } else {
-                    flag = false;
+                    answer[i] = -1;
                     break;
                 }
             }
-            
-            answer[i] = flag ? count : -1;
         }
-        
         
         return answer;
     }
