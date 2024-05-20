0bb68b891997a5a688bea45c5bfc15ea79b857ef
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-05-21 08:50:52 +0900

    [level 1] Title: 대충 만든 자판, Time: 1.42 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
@@ -1,3 +1,10 @@
+/**
+* 1. 최소입력값을 담은 map 생성 o
+* 1.1 중복되는 경우, 더 작은 입력값 처리 o
+* 2. target 단어의 최소 입력 값 처리
+* 2.1 target 단어가 map에 없는 경우 처리
+*/
+
 import java.util.*;
 
 class Solution {
@@ -10,12 +17,14 @@ class Solution {
             for(int j=0; j<keymap[i].length(); j++){
                 char c = keymap[i].charAt(j);
                 
+                // 더 작은 값 대체
                 if(keypad.containsKey(c)){
-                    int cnt = keypad.get(c);
-                    keypad.put(c, Math.min(cnt, j+1));
-                } else{
-                    keypad.put(c, j+1);
+                    if(keypad.get(c) > j+1){
+                        keypad.put(c, j+1);
+                    }
+                    continue;
                 }
+                keypad.put(c, j+1);
             }
         }
         
@@ -23,12 +32,12 @@ class Solution {
             for(int j=0; j<targets[i].length(); j++){
                 char c = targets[i].charAt(j);
                 
-                if(keypad.containsKey(c)){
-                    answer[i] += keypad.get(c);
-                } else {
+                if(!keypad.containsKey(c)){
                     answer[i] = -1;
                     break;
                 }
+                
+                answer[i] += keypad.get(c);
             }
         }
         
