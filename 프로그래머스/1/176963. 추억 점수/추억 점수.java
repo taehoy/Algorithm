bb2996253573cbe9cbd6554162b2d07d6edec489
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-05-21 09:13:09 +0900

    [level 1] Title: 추억 점수, Time: 0.02 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -1,21 +1,35 @@
+/*
+*   1.이름 별 점수 매핑하기 - map 
+*   2.사진 속 이름별 점수 구하기
+*   2.1 사진 속 이름이 없는 경우 처리 - 0점 더하기
+*
+*
+*
+*/
 import java.util.*;
 
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
         int[] answer = new int[photo.length];
         
-        // 맵 생성
-        Map<String, Integer> map = new HashMap<>();
+        // 맵 만들기 <이름, 점수>
+        Map<String, Integer> scoreMap = new HashMap<>();
         
-        // 맵에 이름, 점수 담기
         for(int i=0; i<name.length; i++){
-            map.put(name[i], yearning[i]);
+            scoreMap.put(name[i], yearning[i]);
         }
         
-        // 사진에 있는 이름에 맞춰 점수 추가하기
+        // 사진 속 이름별 점수 구하기
         for(int i=0; i<photo.length; i++){
             for(int j=0; j<photo[i].length; j++){
-                answer[i] += map.getOrDefault(photo[i][j], 0);
+                String nameInPhoto = photo[i][j];
+                // 존재하지 않는 경우
+                if(!scoreMap.containsKey(nameInPhoto)){
+                    answer[i] +=0;
+                    continue;
+                }
+                // 존재할 경우
+                answer[i] += scoreMap.get(nameInPhoto);
             }
         }
         
