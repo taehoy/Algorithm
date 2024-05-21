b11c8b3ffdfbf624b6fac35190d0a13057a8e1c2
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-05-21 10:38:58 +0900

    [level 1] Title: 실패율, Time: 0.39 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,7 +1,17 @@
+/*
+*   1. 실패율 구하기
+*   1.1 머무는 사람 수 구하기
+*   1.2 참가자수와 머문 사람 처리
+*   
+*   
+*
+*/
 import java.util.*;
 
 class Solution {
     public int[] solution(int N, int[] stages) {
+        int[] answer = new int[N];
+        
         int[] clearStages = new int[N+1];
         int[] currentStages = new int[N+1];
         
@@ -14,21 +24,37 @@ class Solution {
         }
         
         // 실패율 map 만들기
-        Map<Integer, Double> map = new HashMap<>();
+        Map<Integer, Double> failPercentMap = new HashMap<>();
         
         for(int i=0; i<N; i++){
             if(clearStages[i] == 0 || currentStages[i] == 0){
-                map.put(i+1, 0.0);
+                failPercentMap.put(i+1, 0.0);
             } else {
-                map.put(i+1, (double)currentStages[i] / (double)clearStages[i]);
+                failPercentMap.put(i+1, (double)currentStages[i] / (double)clearStages[i]);
             }
         }
         
-        // 스테이지 리스트 만들기
-        List<Integer> list = new ArrayList<>(map.keySet()); // 1,2,3,4,5
         
-        list.sort((o1,o2) -> Double.compare(map.get(o2), map.get(o1)));
+        
+        List<Double> failPercentList = new ArrayList<>();
+        
+        for(int i=0; i<failPercentMap.size(); i++){
+            failPercentList.add(failPercentMap.get(i+1));
+        }
+        
+        Collections.sort(failPercentList, Collections.reverseOrder());
+        
+        for(int i=0; i<failPercentList.size(); i++){
+            for(Integer stage : failPercentMap.keySet()){
+                if(failPercentMap.get(stage) == failPercentList.get(i)){
+                    answer[i] = stage;
+                    failPercentMap.put(stage, -0.1);
+                }
+            }
+        }
+        
+        // failPercentList.sort((o1,o2) -> Double.compare(failPercentMap.get(o2), failPercentMap.get(o1)));
         // 리스트 -> 배열
-        return list.stream().mapToInt(i -> i).toArray();
+        return answer;
     }
 }
