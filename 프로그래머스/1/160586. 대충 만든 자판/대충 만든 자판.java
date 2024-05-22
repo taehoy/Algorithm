efaa74bf036c6aff81412dc41f5b39febde5f1a0
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-05-22 10:03:35 +0900

    [level 1] Title: 대충 만든 자판, Time: 2.26 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
@@ -1,30 +1,20 @@
-/**
-* 1. 최소입력값을 담은 map 생성 o
-* 1.1 중복되는 경우, 더 작은 입력값 처리 o
-* 2. target 단어의 최소 입력 값 처리
-* 2.1 target 단어가 map에 없는 경우 처리
-*/
-
 import java.util.*;
 
 class Solution {
     public int[] solution(String[] keymap, String[] targets) {
         int[] answer = new int[targets.length];
         
-        Map<Character, Integer> keypad = new HashMap<>();
+        Map<Character, Integer> map = new HashMap<>();
         
         for(int i=0; i<keymap.length; i++){
             for(int j=0; j<keymap[i].length(); j++){
                 char c = keymap[i].charAt(j);
                 
-                // 더 작은 값 대체
-                if(keypad.containsKey(c)){
-                    if(keypad.get(c) > j+1){
-                        keypad.put(c, j+1);
-                    }
-                    continue;
+                if(map.containsKey(c)){
+                    map.put(c, Math.min(j+1, map.get(c)));
+                } else {
+                    map.put(c, j+1);
                 }
-                keypad.put(c, j+1);
             }
         }
         
@@ -32,12 +22,12 @@ class Solution {
             for(int j=0; j<targets[i].length(); j++){
                 char c = targets[i].charAt(j);
                 
-                if(!keypad.containsKey(c)){
+                if(!map.containsKey(c)){
                     answer[i] = -1;
                     break;
+                } else {
+                    answer[i] += map.get(c);
                 }
-                
-                answer[i] += keypad.get(c);
             }
         }
         
