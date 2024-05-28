e820d3ca2b5572408b538488f9bbf42bcdef5fef
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-05-28 09:44:38 +0900

    [level 2] Title: 의상, Time: 0.12 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -7,16 +7,11 @@ class Solution {
         Map<String, Integer> map = new HashMap<>();
         
         for(String[] cloth : clothes){
-            if(map.containsKey(cloth[1])){
-                map.put(cloth[1], map.get(cloth[1])+1);
-            } else{
-                map.put(cloth[1], 1);
-            }
+            map.put(cloth[1], map.getOrDefault(cloth[1],0)+1);
         }
         
-        // 모든 조합
-        for(Integer i : map.values()){
-            answer *= (i+1);
+        for(Integer n : map.values()){
+            answer *= (n+1);
         }
         
         return answer-1;
