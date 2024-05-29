cd0b9064d4e206e14434d6062584f47e0a09dcf5
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-05-29 17:31:39 +0900

    [level 2] Title: 오픈채팅방, Time: 360.53 ms, Memory: 396 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -1,52 +1,66 @@
-import java.util.*;
-import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 class Solution {
-    public String[] solution(String[] record) throws IOException {
-        // map<아이디, 닉네임>
-        Map<String, String> map = new HashMap<>();
-        String order = "";
-        String userId = "";
-        String nickname = "";
-        String[] strings = {};
-        
-        for(int i=0; i<record.length; i++){
-            strings = record[i].split(" "); // 입력 문장 나누기
-            
-            // 명령어
-            order = strings[0];
-            userId = strings[1];
-            
-            if(order.equals("Enter") || order.equals("Change")){
-                nickname = strings[2];
-                map.put(userId, nickname);
-            } 
+    private static final String ENTER_FORMAT = "%s님이 들어왔습니다.";
+    private static final String LEAVE_FORMAT = "%s님이 나갔습니다.";
+
+    private HashMap<String, UserInfo> userMap = new HashMap<>();
+
+    private class UserInfo {
+        public String userId;
+        public String nickName;
+
+        public UserInfo(String userId, String nickName) {
+            this.userId = userId;
+            this.nickName = nickName;
         }
-        
-        List<String> list = new ArrayList<>();
-        
-        // 출력
-        for(int i=0; i<record.length; i++){
-            strings = record[i].split(" "); // 입력 문장 나누기
-            
-            // 명령어
-            order = strings[0];
-            userId = strings[1];
-            nickname = map.get(userId);
-            
-            if(order.equals("Enter")){
-                list.add(nickname + "님이 들어왔습니다.");
-            } else if(order.equals("Leave")){
-                list.add(nickname + "님이 나갔습니다.");
+
+    }
+
+    private class Command {
+        public char command;
+        public String userId;
+
+        public Command(char command, String userName) {
+            this.command = command;
+            this.userId = userName;
+        }
+    }
+
+
+    public String[] solution(String[] records) {
+        ArrayList<Command> commandList = new ArrayList<>();
+
+        for (String record : records) {
+            String[] split = record.split(" ");
+            String command = split[0];
+            String userId = split[1];
+            String nickName = null;
+
+            switch(command.charAt(0)) {
+                case 'E': // Enter
+                    nickName = split[2];
+                    if(userMap.containsKey(userId) == false) {
+                        userMap.put(userId, new UserInfo(userId, nickName));
+                    } else {
+                        userMap.get(userId).nickName = nickName;
+                    }
+
+                    commandList.add(new Command(command.charAt(0), userId));
+                    break;
+                case 'L': // Leave
+                    commandList.add(new Command(command.charAt(0), userId));
+                    break;
+                case 'C': // Change
+                    nickName = split[2];
+                    userMap.get(userId).nickName = nickName;
+                    break;
             }
         }
-        
-        String[] answer = new String[list.size()];
-        
-        for(int i=0; i<list.size(); i++){
-            answer[i] = list.get(i);
-        }
-        
-        return answer;
+
+        return commandList.stream()
+                .map(cmd -> String.format( cmd.command == 'E' ? ENTER_FORMAT : LEAVE_FORMAT , userMap.get(cmd.userId).nickName))
+                .toArray(ary -> new String[commandList.size()]);
     }
 }
