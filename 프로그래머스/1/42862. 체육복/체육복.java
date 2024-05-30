4b6467c8ace667eafb5894b66212e38b64fd8281
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-05-30 14:50:25 +0900

    [level 1] Title: 체육복, Time: 0.07 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -4,29 +4,29 @@ class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = 0;
         
-        Set<Integer> lostSet = new HashSet<>();
+        // 여분, 도난 리스트 생성
         Set<Integer> reserveSet = new HashSet<>();
+        Set<Integer> lostSet = new HashSet<>();
         
         for(int num : reserve){
             reserveSet.add(num);
         }
         
-        for(int num: lost){
+        for(int num : lost){
             if(reserveSet.contains(num)){
                 reserveSet.remove(num);
-            } else{
-                lostSet.add(num);
+                continue;
             }
+            lostSet.add(num);
         }
         
+        // 여분 있는 사람 중 도난 당한 사람 빌려주기
         for(int num : reserveSet){
-            if(lostSet.contains(num-1)){
-                lostSet.remove(num-1);
-            } else if(lostSet.contains(num+1)){
-                lostSet.remove(num+1);
-            }
+            if(lostSet.contains(num-1)) lostSet.remove(num-1);
+            else if(lostSet.contains(num+1)) lostSet.remove(num+1);
         }
         
+        // 수업 참여인원 세기
         return n-lostSet.size();
     }
 }
