25b502563d733778994fa5df950dfaacd1dfa2ae
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-05-31 15:15:58 +0900

    [level 2] Title: 문자열 압축, Time: 2.27 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -1,23 +1,23 @@
-import java.util.*;
-
 class Solution {
     public int solution(String s) {
+        
         int answer = s.length();
         
         for(int i=1; i<=s.length()/2; i++){
             StringBuilder sb = new StringBuilder();
             
-            int zipLength = 1;
             String zipStr = s.substring(0,i);
+            int zipLength = 1;
             
             for(int j=i; j<=s.length(); j+=i){
-                String nextStr = s.substring(j, (j+i > s.length()) ? s.length() : j+i);
+                String nextStr = s.substring(j, (j+i >s.length()) ? s.length() : j+i);
                 
-                if(zipStr.equals(nextStr)) zipLength++;
-                else {
-                    sb.append((zipLength != 1 ? zipLength : "") + zipStr);
+                if(zipStr.equals(nextStr)){
+                    zipLength++;
+                } else {
+                    sb.append(((zipLength != 1) ? zipLength : "") + zipStr);
                     zipStr = nextStr;
-                    zipLength =1;
+                    zipLength = 1;
                 }
             }
             
@@ -25,7 +25,6 @@ class Solution {
             
             answer = Math.min(answer, sb.length());
         }
-        
         return answer;
     }
 }
