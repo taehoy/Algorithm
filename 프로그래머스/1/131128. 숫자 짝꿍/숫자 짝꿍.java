5c5c1727de28b5513573c497c86315deabcaac9e
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-05-31 15:54:42 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.03 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -1,27 +1,30 @@
 class Solution {
     public String solution(String X, String Y) {
+
         StringBuilder sb = new StringBuilder();
         
         int[] arrX = new int[10];
         int[] arrY = new int[10];
         
-        for(int i=0; i<X.length(); i++){
-            arrX[X.charAt(i)-'0']++;
+        for(char c : X.toCharArray()){
+            int idx = (int)c-'0';
+            arrX[idx]++;
         }
         
-        for(int i=0; i<Y.length(); i++){
-            arrY[Y.charAt(i)-'0']++;
+        for(char c : Y.toCharArray()){
+            int idx = (int)c-'0';
+            arrY[idx]++;
         }
         
         for(int i=9; i>=0; i--){
-            while(arrX[i] >=1 && arrY[i] >= 1){
-                sb.append(String.valueOf(i));
+            while(arrX[i] >=1  && arrY[i] >= 1){
+                sb.append(i);
                 arrX[i]--;
                 arrY[i]--;
             }
         }
         
-        if(sb.length() == 0) return "-1";
+        if(sb.length() ==0) return "-1";
         if(sb.toString().charAt(0) == '0') return "0";
         
         return sb.toString();
