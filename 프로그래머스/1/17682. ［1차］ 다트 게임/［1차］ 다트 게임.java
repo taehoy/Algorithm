68c92a9a1ddfd4fe14eea8292063301d869224c9
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-05-31 15:28:13 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.05 ms, Memory: 70.8 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -1,36 +1,34 @@
 class Solution {
     public int solution(String dartResult) {
+        int answer = 0;
+        
         char[] arr = dartResult.toCharArray();
         int[] scores = new int[3];
-        int index = -1;
+        int idx = -1;
         
         for(int i=0; i<arr.length; i++){
             if('0' <= arr[i] && arr[i] <= '9'){
-                index++;    
+                idx++;
                 if(arr[i+1] == '0'){
-                    scores[index] = 10;
                     i++;
+                    scores[idx] = 10;
                     continue;
                 }
-                
-                scores[index] = Integer.parseInt(String.valueOf(arr[i]));
+                scores[idx] = Integer.parseInt(String.valueOf(arr[i]));
             }
             
-            
-            // 영역, 옵션
-            if(arr[i] == 'D'){
-                scores[index] = (int)Math.pow(scores[index], 2);
-            } else if(arr[i] == 'T'){
-                scores[index] = (int)Math.pow(scores[index], 3);
-            } else if(arr[i] == '*'){
-                scores[index] *= 2;
-                if(index-1 >= 0){
-                    scores[index-1] *= 2;
+            if(arr[i] == 'D') scores[idx] = (int)Math.pow(scores[idx], 2);
+            else if(arr[i] == 'T') scores[idx] = (int)Math.pow(scores[idx], 3);
+            else if(arr[i] == '*'){
+                scores[idx] *= 2;
+                if(idx-1 >= 0){
+                    scores[idx-1] *= 2;
                 }
-            } else if(arr[i] == '#'){
-                scores[index] *= -1;
             }
+            else if(arr[i] == '#') scores[idx] *= -1;
+            
         }
+        
         return scores[0] + scores[1] + scores[2];
     }
 }
