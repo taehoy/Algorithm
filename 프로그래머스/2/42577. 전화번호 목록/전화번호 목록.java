1383679dfcd0585c77bc6b2477b5c42bcf8f9beb
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-06-03 10:30:10 +0900

    [level 2] Title: 전화번호 목록, Time: 202.48 ms, Memory: 132 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -4,10 +4,10 @@ class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
         
-        Map<String, Integer> map = new HashMap<>();
+        Map<String,Integer> map = new HashMap<>();
         
-        for(int i=0; i<phone_book.length; i++){
-            map.put(phone_book[i], 1);
+        for(String phone : phone_book){
+            map.put(phone, 1);
         }
         
         for(int i=0; i<phone_book.length; i++){
@@ -18,6 +18,6 @@ class Solution {
             }
         }
         
-        return true;
+        return answer;
     }
 }
