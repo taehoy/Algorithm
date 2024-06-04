5384bb604ef11420ca28af39f7b1b5e38eb545d8
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-06-04 19:47:51 +0900

    [level 2] Title: H-Index, Time: 0.46 ms, Memory: 67.5 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -3,7 +3,6 @@ import java.util.*;
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
-        int cnt = 0; 
         
         Arrays.sort(citations);
         int n = citations.length;
@@ -12,13 +11,13 @@ class Solution {
             return 0;
         }
         
+        
+        int idx = 0;
         for(int i=n-1; i>=0; i--){
-            if(citations[i] <= cnt){
-                break;
-            }
-            cnt++;
+            if(idx >= citations[i]) break;
+            idx++;
         }
         
-        return cnt;
+        return idx;
     }
 }
