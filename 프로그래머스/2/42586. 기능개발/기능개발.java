03e8e437d48afc2803be2bdff4e653945389fa45
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-06-04 20:12:55 +0900

    [level 2] Title: 기능개발, Time: 0.12 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -4,35 +4,31 @@ class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         Queue<Integer> q = new LinkedList<>();
         
-        // 남은 일수 구한다.
         for(int i=0; i<progresses.length; i++){
             if((100 - progresses[i]) % speeds[i] == 0){
                 q.offer((100 - progresses[i]) / speeds[i]);
-                continue;
+            } else {
+                q.offer((100 - progresses[i]) / speeds[i] +1);
             }
-            q.offer((100 - progresses[i]) / speeds[i] +1);
-            
         }
         
-        int before = q.poll(); // 이전 일수 
-        int cnt = 1;
+        int before = q.poll();
+        int count =1;
+        
         List<Integer> list = new ArrayList<>();
         
-        // 남은 일수 비교
         while(!q.isEmpty()){
-            // 함께 배포
             if(before >= q.peek()){
                 q.poll();
-                cnt++;
-            } else { // 따로 배포
-                list.add(cnt);
-                cnt = 1;
+                count++;
+            } else {
+                list.add(count);
+                count = 1;
                 before = q.poll();
             }
         }
         
-        // 마지막 배포
-        list.add(cnt);
+        list.add(count);
         
         int[] answer = new int[list.size()];
         
