bd833e075c6b745e59e3baa1d6cb602cd0ddf890
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-06-04 14:27:48 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -2,12 +2,12 @@ class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
         
-        for(int col=3; col<=5000; col++){
-            for(int row=3; row<=col; row++){
-                int boundary = (col+row -2) *2;
-                int center = col*row - boundary;
+        for(int col = 3; col <=5000; col++){
+            for(int row = 3; row<=col; row++){
+                int boundary = (col+row-2) * 2;
+                int center = (col * row) - boundary;
                 
-                if(boundary == brown && center == yellow){
+                if(brown == boundary && yellow == center){
                     answer[0] = col;
                     answer[1] = row;
                     
