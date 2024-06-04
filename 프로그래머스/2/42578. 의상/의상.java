5a4d78b293ebc0ba11733eb9899ce4492fffc912
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-06-04 14:42:28 +0900

    [level 2] Title: 의상, Time: 0.22 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -7,11 +7,14 @@ class Solution {
         Map<String, Integer> map = new HashMap<>();
         
         for(String[] cloth : clothes){
-            map.put(cloth[1], map.getOrDefault(cloth[1],0)+1);
+            map.put(cloth[1], map.getOrDefault(cloth[1],0) +1);
         }
         
-        for(Integer n : map.values()){
-            answer *= (n+1);
+        System.out.println(map);
+        
+        // 없는거까지 포함해서 개수 경우의 수 계산
+        for(int num : map.values()){
+            answer *= (num+1);
         }
         
         return answer-1;
