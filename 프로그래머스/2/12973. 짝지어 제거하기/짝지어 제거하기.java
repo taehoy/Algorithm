5277346481da47a4414424dc694f2fdefdad77ac
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-06-05 15:20:09 +0900

    [level 2] Title: 짝지어 제거하기, Time: 102.63 ms, Memory: 60.8 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -5,7 +5,7 @@ class Solution
     public int solution(String s)
     {
         char[] ch = s.toCharArray();
-
+        
         Stack<Character> stack = new Stack<>();
         
         for(char c : ch){
@@ -13,6 +13,7 @@ class Solution
                 stack.push(c);
                 continue;
             }
+                
             
             if(stack.peek() == c){
                 stack.pop();
@@ -20,7 +21,7 @@ class Solution
                 stack.push(c);
             }
         }
-
+        
         return stack.isEmpty() ? 1 : 0;
     }
 }
