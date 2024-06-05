d26db2cd8492532acb45a711616b46dd635784be
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-06-05 13:47:16 +0900

    [level 2] Title: 프로세스, Time: 0.90 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,27 +2,47 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
-        int answer = 0;
+        Queue<Process> queue = new LinkedList<>();
         
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        
-        for(int i : priorities){
-            pq.offer(i);
+        // 큐를 (인덱스, 우선순위) 형태로 변환합니다.
+        for (int i = 0; i < priorities.length; i++) {
+            queue.offer(new Process(i, priorities[i]));
         }
         
-        System.out.println(pq);
+        int printOrder = 0;  // 몇 번째로 출력되는지 카운트
         
-        while(!pq.isEmpty()){
-            for(int i=0; i<priorities.length; i++){
-                if(priorities[i] == pq.peek()){
-                    pq.poll();
-                    answer++;
-                    
-                    if(location == i) return answer;
+        while (!queue.isEmpty()) {
+            Process current = queue.poll();
+            
+            // 현재 프로세스보다 우선순위가 높은 프로세스가 있는지 확인합니다.
+            boolean hasHigherPriority = false;
+            for (Process p : queue) {
+                if (p.priority > current.priority) {
+                    hasHigherPriority = true;
+                    break;
+                }
+            }
+            
+            if (hasHigherPriority) {
+                queue.offer(current);  // 다시 큐에 넣습니다.
+            } else {
+                printOrder++;  // 프로세스를 실행(출력)합니다.
+                if (current.index == location) {
+                    return printOrder;
                 }
             }
         }
         
-        return answer;
+        return -1;  // 오류가 발생할 경우 -1을 반환합니다.
+    }
+    
+    static class Process {
+        int index;
+        int priority;
+        
+        Process(int index, int priority) {
+            this.index = index;
+            this.priority = priority;
+        }
     }
 }
