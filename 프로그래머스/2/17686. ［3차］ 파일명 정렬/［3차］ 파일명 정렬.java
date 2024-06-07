5ece332958793be9a4a8b6fa1ed49b4c3f202458
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-06-07 15:08:11 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 55.79 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -1,19 +1,20 @@
 import java.util.*;
 
 class Solution {
-    private static String[] seperateFile(String file){
+    private static String[] separateFilename(String file){
         file = file.toUpperCase();
         
-        String head = file.split("[0-9]")[0];
-        String number = file.substring(head.length());
-        return new String[]{head, number};
+        String header = file.split("[0-9]")[0]; // 헤더
+        String number = file.substring(header.length());// 넘버
+        
+        return new String[]{header, number};
     }
     
-    private static String validateNumber(String number){
+    private static String getNumber(String number){
         StringBuilder sb = new StringBuilder();
         
         for(char c : number.toCharArray()){
-            if(Character.isDigit(c) && sb.length()<=5){
+            if(Character.isDigit(c) && sb.length() <= 5){
                 sb.append(c);
             } else {
                 return sb.toString();
@@ -24,20 +25,20 @@ class Solution {
     }
     
     public String[] solution(String[] files) {
-        
         Arrays.sort(files, new Comparator<String>(){
-            @Override
-            public int compare(String o1, String o2){
-                String[] arr1 = seperateFile(o1);
-                String[] arr2 = seperateFile(o2);
+           @Override
+            public int compare(String s1, String s2){
+                String[] arr1 = separateFilename(s1);
+                String[] arr2 = separateFilename(s2);
                 
                 int result = arr1[0].compareTo(arr2[0]);
                 
-                if(result ==0){
-                    int num1 = Integer.parseInt(validateNumber(arr1[1]));
-                    int num2 = Integer.parseInt(validateNumber(arr2[1]));
+                // 문자가 같을 경우, 숫자 비교
+                if(result == 0){
+                    int num1 = Integer.parseInt(getNumber(arr1[1]));
+                    int num2 = Integer.parseInt(getNumber(arr2[1]));
                     
-                    return num1-num2;
+                    return num1- num2;
                 }
                 
                 return result;
