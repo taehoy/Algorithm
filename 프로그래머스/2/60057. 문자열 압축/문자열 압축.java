18bd12ce1a1c9f266bf5f16a5fdb78d135938f26
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-06-07 15:45:02 +0900

    [level 2] Title: 문자열 압축, Time: 1.21 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -1,23 +1,22 @@
 class Solution {
     public int solution(String s) {
-        
         int answer = s.length();
+        
         
         for(int i=1; i<=s.length()/2; i++){
             StringBuilder sb = new StringBuilder();
-            
             String zipStr = s.substring(0,i);
             int zipLength = 1;
             
             for(int j=i; j<=s.length(); j+=i){
-                String nextStr = s.substring(j, (j+i >s.length()) ? s.length() : j+i);
+                String nextStr = s.substring(j, (j+i) > s.length() ? s.length() : j+i);
                 
-                if(zipStr.equals(nextStr)){
-                    zipLength++;
-                } else {
+                // 다음 문자와 압축 문자가 같으면 압축 횟수 증가
+                if(zipStr.equals(nextStr)) zipLength++;
+                else{
                     sb.append(((zipLength != 1) ? zipLength : "") + zipStr);
-                    zipStr = nextStr;
                     zipLength = 1;
+                    zipStr = nextStr;
                 }
             }
             
@@ -26,5 +25,6 @@ class Solution {
             answer = Math.min(answer, sb.length());
         }
         return answer;
+        
     }
 }
