112f8b820ab677fa1c17bc626b2e9ed93164fafc
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-06-07 14:55:16 +0900

    [level 2] Title: 구명보트, Time: 10.02 ms, Memory: 55.9 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -3,6 +3,7 @@ import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
+        
         Arrays.sort(people);
         
         int left = 0;
