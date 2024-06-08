076f62c7036744a3baacfe2e9eb8871e49fba8f8
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-06-08 19:34:23 +0900

    [level 2] Title: 조이스틱, Time: 0.06 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -7,6 +7,7 @@ class Solution {
             answer += Math.min(name.charAt(i) - 'A', 26 - (name.charAt(i) - 'A')); //상,하 알파벳 맞추기
             if (i < name.length() - 1 && name.charAt(i + 1) == 'A') {
                 int endA = i + 1;
+                
                 while(endA < name.length() && name.charAt(endA) == 'A')
                     endA++;
                 move = Math.min(move, i * 2 + (name.length() - endA));// 오른쪽으로 갔다 다시 왼쪽으로 꺾기
