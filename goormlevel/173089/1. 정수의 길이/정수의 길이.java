bb6e7617ca5c69ed6454aa2a01394f3a8c71d8a7
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-06-12 10:30:28 +0900

    [goormlevel] Title: 정수의 길이, Time: 0.10 ms, Memory: 31.39 MB, Difficulty: 1 -BaekjoonHub

---
+++ goormlevel/173089/1. 정수의 길이/정수의 길이.java
@@ -0,0 +1,10 @@
+import java.io.*;
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String input = br.readLine();
+		
+		System.out.println(input.length());
+	}
+}
