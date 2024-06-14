341e67f0ae529dd44ca93548fa1edf0aee59c5dd
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-06-14 14:23:30 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 2.12 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -2,17 +2,21 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] food) {
-        String answer = "";
         StringBuilder sb = new StringBuilder();
         
+        int n = 1;
         for(int i=1; i<food.length; i++){
-            for(int j=0; j< food[i] / 2; j++){
-                sb.append(String.valueOf(i));
+            int times = food[i]/2;
+            
+            for(int j=0; j<times; j++){
+                sb.append(n);
             }
+            n++;
         }
-        answer = sb + "0";
-        answer += sb.reverse();
         
-        return answer;
+        String s = sb + "0";
+        s += sb.reverse();
+
+        return s;
     }
 }
