f68b783637856a19ec84835c10c00fe06ec88f2d
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-06-17 17:25:01 +0900

    [Bronze II] Title: 다이얼, Time: 76 ms, Memory: 11548 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.java
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -1,52 +1,39 @@
-import java.util.Scanner;
- 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
 public class Main {
-	public static void main(String[] args) {
-    
-		Scanner in = new Scanner(System.in);
-		
-		String s = in.nextLine();
- 
-		int count = 0;
-		int k = s.length();
-        
-		for(int i = 0; i < k; i++) {
-        
-			switch(s.charAt(i)) {
-            
-			case 'A' : case 'B': case 'C' : 
-				count += 3;
-				break;
-                
-			case 'D' : case 'E': case 'F' :
-				count += 4; 
-				break;
-                
-			case 'G' : case 'H': case 'I' :
-				count += 5; 
-				break;
-                
-			case 'J' : case 'K': case 'L' : 
-				count += 6;
-				break;
-                
-			case 'M' : case 'N': case 'O' :
-				count += 7;
-				break;
-                
-			case 'P' : case 'Q': case 'R' : case 'S' : 
-				count += 8; 
-				break;
-                
-			case 'T' : case 'U': case 'V' : 
-				count += 9;
-				break;
-                
-			case 'W' : case 'X': case 'Y' : case 'Z' : 
-				count += 10;
-				break;
-			}
-		}		
-		System.out.print(count);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Map<String, Integer> map = new HashMap<>();
+
+        map.put("ABC", 3);
+        map.put("DEF", 4);
+        map.put("GHI", 5);
+        map.put("JKL", 6);
+        map.put("MNO", 7);
+        map.put("PQRS", 8);
+        map.put("TUV", 9);
+        map.put("WXYZ", 10);
+
+        String s = br.readLine();
+        int sum = 0;
+
+        for(int i=0; i<s.length(); i++){
+            String s1 = s.substring(i, i+1);
+
+            for(String s2 : map.keySet()){
+                if(s2.contains(s1)){
+                    sum +=map.get(s2);
+                    continue;
+                }
+            }
+        }
+
+        System.out.println(sum);
+    }
 }
