6b285d6ce1c4f45b9e8603b4084004943cc801ed
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-06-17 16:32:43 +0900

    [Bronze V] Title: 문자와 문자열, Time: 80 ms, Memory: 11456 KB -BaekjoonHub

--- 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -1,14 +1,14 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		
-		String st = sc.next();
-		int n = sc.nextInt()-1;
-		
-		System.out.println(st.charAt(n));
-		
-		sc.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(s.substring(n-1,n));
+    }
 }
