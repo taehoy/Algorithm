71f63da9cf17357c89e3e68d04c03a449bc82ea1
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-06-17 17:16:03 +0900

    [Bronze II] Title: 상수, Time: 76 ms, Memory: 11512 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -1,20 +1,23 @@
-import java.util.Scanner;
- 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
- 
-	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);
-        
-		int A = in.nextInt();
-		int B = in.nextInt();
-        
-        in.close();
-        
-		A = Integer.parseInt(new StringBuilder().append(A).reverse().toString());
-		B = Integer.parseInt(new StringBuilder().append(B).reverse().toString());
-		
-		System.out.print(A > B ? A : B);
-	
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder sb = new StringBuilder();
+        String s = br.readLine();
+
+        sb.append(s);
+        sb.reverse();
+
+        String s1 = sb.toString();
+        String[] arr = s1.split(" ");
+        int n1 = Integer.parseInt(arr[0]);
+        int n2 = Integer.parseInt(arr[1]);
+
+        System.out.println(Math.max(n1, n2));
+    }
 }
