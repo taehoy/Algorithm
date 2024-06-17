59509d586e9eee78be611c4b0b2413809358dccd
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-06-17 16:36:42 +0900

    [Bronze V] Title: 문자열, Time: 72 ms, Memory: 11404 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.java
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -1,16 +1,25 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < n; i++) {
+
+        int n  = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<n; i++){
             String s = br.readLine();
-            sb.append(s.charAt(0));
-            sb.append(s.charAt(s.length()- 1) + "\n");
+
+            if(s.length()==1){
+                System.out.println(s+s);
+                continue;
+            }
+
+            String s1 = s.substring(0,1);
+            String s2 = s.substring(s.length()-1, s.length());
+
+            System.out.println(s1+s2);
         }
-        System.out.print(sb);
     }
 }
