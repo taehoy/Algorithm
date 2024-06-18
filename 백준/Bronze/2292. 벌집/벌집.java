1e0dd183369d4fc8b6a915a9f6205a9c0847d2de
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-06-18 11:56:45 +0900

    [Bronze II] Title: 벌집, Time: 68 ms, Memory: 11488 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.java
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -3,27 +3,21 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+    static int getNumOfRooms(int num){
+        int cnt = 1;
+
+        while(num > 1){
+            num -= (6 * cnt);
+            cnt++;
+        }
+        return cnt;
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        int start = 2;
-        int end = 7;
-        int num = 6;            // 육각형
+        System.out.println(getNumOfRooms(N));
 
-        for(int i=0; ; i++){
-            if(N == 1){
-                System.out.println(1);
-                break;
-            }
-
-            if(start <= N && N <= end){
-                System.out.println(i+2);
-                break;
-            }
-
-            start = start + num*(i+1);
-            end = end + num*(i+2);
-        }
     }
 }
