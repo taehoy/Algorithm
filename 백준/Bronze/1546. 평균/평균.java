0afa80015fdf6a39d6878a4f938aade4768807b9
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-06-18 12:50:23 +0900

    [Bronze I] Title: 평균, Time: 72 ms, Memory: 11916 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,24 +1,37 @@
-import java.util.Arrays;
-import java.util.Scanner;
- 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-	public static void main(String[] args) {
- 
-		Scanner sc = new Scanner(System.in);
- 
-		double arr[] = new double[sc.nextInt()];
-		
-		for(int i = 0; i < arr.length; i++) {
-			arr[i] = sc.nextDouble();
-		}
-		sc.close();
-		
-		double sum = 0;
-		Arrays.sort(arr);
-		
-		for(int i = 0; i < arr.length; i++) {
-			sum += ((arr[i] / arr[arr.length-1]) * 100);
-		}
-		System.out.print(sum / arr.length);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int max = 0;
+
+        String s = br.readLine();
+        String[] arr = s.split(" ");
+
+        // 최대값 구하기
+        for(int i=0; i<N; i++){
+            int num = Integer.parseInt(arr[i]);
+            max = Math.max(max, num);
+        }
+
+        // 모든 점수 변경
+        double sum = 0;
+
+        for(int i=0; i<N; i++){
+            int currScore = Integer.parseInt(arr[i]);
+            double updateScore = (double)currScore / max * 100;
+
+            sum += updateScore;
+        }
+
+        // 평균구하기
+        double result = sum / N;
+
+        System.out.println(result);
+    }
 }
