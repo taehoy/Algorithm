0a52acfc83264dec50636cc08d9172b51ee6654d
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-06-21 09:23:29 +0900

    [Bronze I] Title: FizzBuzz, Time: 80 ms, Memory: 11616 KB -BaekjoonHub

--- 백준/Bronze/28702. FizzBuzz/FizzBuzz.java
+++ 백준/Bronze/28702. FizzBuzz/FizzBuzz.java
@@ -1,25 +1,24 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+public class Main{
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        
         for(int i = 3; i>0; i--){
             String s = br.readLine();
-
+            
             if(s.matches("-?\\d+(\\.\\d+)?")){
-                int n = Integer.parseInt(s) + i; // 네번째 수
-
+                int n = Integer.parseInt(s) + i;
+                
                 if(n % 3 == 0 && n % 5 == 0) System.out.println("FizzBuzz");
                 else if(n % 3 == 0 && n % 5 != 0) System.out.println("Fizz");
                 else if(n % 5 == 0 && n % 3 != 0) System.out.println("Buzz");
                 else System.out.println(n);
-
+                
                 break;
             }
-
         }
         br.close();
     }
