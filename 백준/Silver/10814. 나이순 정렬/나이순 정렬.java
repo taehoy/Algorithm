e26925d2b37740064608237df4e6cbc86a9073d9
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-06-24 09:57:34 +0900

    [Silver V] Title: 나이순 정렬, Time: 460 ms, Memory: 61704 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,44 +1,34 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        // 입력 값 받기
+
+        // 카운팅 정렬 이용
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
 
-        String[][] arr = new String[N][2];
+        StringBuilder[] p = new StringBuilder[201];
 
-        for(int i=0; i<N; i++){
-            String[] temp = br.readLine().split(" ");
-            arr[i][0] = temp[0];
-            arr[i][1] = temp[1];
+        for(int i=0; i<p.length; i++){
+            p[i] = new StringBuilder();
         }
 
-        // 나이순 정렬
-        Arrays.sort(arr, new Comparator<String[]>() {
-            @Override
-            public int compare(String[] o1, String[] o2) {
-                int age1 = Integer.parseInt(o1[0]);
-                int age2 = Integer.parseInt(o2[0]);
+        for(int i=0; i<N; i++){
+            String[] arr = br.readLine().split(" ");
+            int age = Integer.parseInt(arr[0]);
+            String name = arr[1];
 
-                int result = age1-age2;
+            p[age].append(age).append(' ').append(name).append("\n");
+        }
 
-                return result;
-            }
-        });
-
-        // 출력
         StringBuilder sb = new StringBuilder();
-
-        for(int i=0; i<N; i++){
-            sb.append(arr[i][0]).append(" ").append(arr[i][1]).append("\n");
+        for(StringBuilder var : p){
+            sb.append(var);
         }
-
         System.out.println(sb);
+
     }
 }
