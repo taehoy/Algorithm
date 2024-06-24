e8c8a93d94509e75333e03f194eee5faa54b60e8
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-06-24 16:53:37 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 880 ms, Memory: 55824 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -3,19 +3,22 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
+        int N =Integer.parseInt(br.readLine());
 
         String[][] arr = new String[N][2];
 
+        StringTokenizer st;
+
         for(int i=0; i<N; i++){
-            String[] temp = br.readLine().split(" ");
-            arr[i][0] = temp[0];
-            arr[i][1] = temp[1];
+            st = new StringTokenizer(br.readLine(), " ");
+            arr[i][0] = st.nextToken();
+            arr[i][1] = st.nextToken();
         }
 
         Arrays.sort(arr, new Comparator<String[]>() {
@@ -24,12 +27,12 @@ public class Main {
                 int x1 = Integer.parseInt(o1[0]);
                 int x2 = Integer.parseInt(o2[0]);
 
-                int result = x1-x2;
+                int result = x1 - x2;
 
-                // x좌표가 같을 경우
-                if(result == 0) {
+                if(result == 0){
                     int y1 = Integer.parseInt(o1[1]);
                     int y2 = Integer.parseInt(o2[1]);
+
                     return y1 - y2;
                 }
 
