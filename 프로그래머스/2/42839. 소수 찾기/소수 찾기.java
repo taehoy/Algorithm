1d3a455ac2b460f9275ddacd9e3a20a67fdfa902
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-06-25 11:54:46 +0900

    [level 2] Title: 소수 찾기, Time: 30.80 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,47 +1,60 @@
-import java.util.*;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
-class Solution {
-    
-    boolean[] visited;
-    Set<Integer> map;
-    
-    public int solution(String numbers) {
-        visited = new boolean[numbers.length()];
-        map = new HashSet<>();
-        
-        backTracking(0, numbers, "");
-        return map.size();
+public class Solution {
+    Set<Integer> numberSet = new HashSet<>();
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.solution("011"));
     }
-    
-    public void backTracking(int depth, String numbers, String current) {
-        if (depth == numbers.length()) {
-            return;
+
+    public void recursive(String comb, String others){
+        // 1. 빈 문자열이 아닌 경우 처리
+        if(!comb.equals("")){
+            numberSet.add(Integer.parseInt(comb));
         }
-        
-        for (int i = 0; i < numbers.length(); i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                String number = current + numbers.charAt(i);
-                
-                if (check(number)) {
-                    map.add(Integer.parseInt(number));
-                }
-                
-                backTracking(depth + 1, numbers, number);
-                visited[i] = false;
+
+        // 2. recursive호출
+        for(int i=0; i<others.length(); i++){
+            recursive(comb + others.charAt(i), others.substring(0,i) + others.substring(i+1));
+        }
+    }
+
+    public int solution(String numbers) {
+        // 1. 모든 수의 조합
+        recursive("", numbers);
+
+        // 2. 소수 개수 구하기
+        int count =0;
+        Iterator<Integer> it = numberSet.iterator();
+        while(it.hasNext()){
+            int num = it.next();
+            if(isPrime(num)){
+                count++;
             }
         }
+
+        // 3. 소수 개수 출력하기
+
+        return count;
     }
-    
-    public boolean check(String number) {
-        int num = Integer.parseInt(number);
-        
-        for (int i = 2; i <= num; i++) {
-            if (i != num && num % i == 0)
+
+    private boolean isPrime(int num) {
+        // 0과 1이면 패스 처리
+        if(num == 0 || num == 1)
+            return false;
+
+        // limit 계산
+        int lim = (int)Math.sqrt(num);
+
+        // 소수 여부 확인
+        for(int i=2; i<=lim; i++){
+            if(num % i == 0)
                 return false;
         }
-        
-        if (num <= 1) return false;
+
         return true;
     }
 }
