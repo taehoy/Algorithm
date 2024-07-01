44e20711d77adb80279eefc6586fa220691aabb1
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-07-01 10:04:41 +0900

    [Silver IV] Title: 괄호, Time: 68 ms, Memory: 11692 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -6,19 +6,26 @@ import java.util.Stack;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-        for(int i=0; i<T; i++){
-            System.out.println(solution(br.readLine(), br));
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<N; i++){
+            String s = br.readLine();
+            sb.append(solution(s)).append("\n");
         }
 
+        System.out.println(sb);
     }
 
-    private static String solution(String str, BufferedReader br) throws IOException {
+    private static String solution(String s){
         Stack<Character> stack = new Stack<>();
-        for(int i=0; i<str.length(); i++){
-            if(str.charAt(i) == '('){
-                stack.push('(');
+
+        for(int j=0; j<s.length(); j++){
+            char c = s.charAt(j);
+
+            if(c == '('){
+                stack.push(c);
             } else {
                 if(stack.isEmpty()){
                     return "NO";
@@ -27,11 +34,7 @@ public class Main {
             }
         }
 
-        if(!stack.isEmpty()){
-            return "NO";
-        }
-
-        return "YES";
-
+        return stack.isEmpty() ? "YES" : "NO";
     }
+
 }
