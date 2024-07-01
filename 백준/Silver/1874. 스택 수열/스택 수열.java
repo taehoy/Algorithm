5c0eaf14057e2fb6bd7a084667e6d380178ab5a4
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-07-01 20:14:41 +0900

    [Silver II] Title: 스택 수열, Time: 264 ms, Memory: 28656 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -7,36 +7,36 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Stack<Integer> stack = new Stack<>();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
 
-        // 기준숫자
+        int N =Integer.parseInt(br.readLine());
+
         int start = 0;
+
         // N번 반복
         for(int i=0; i<N; i++){
-            int value = Integer.parseInt(br.readLine());
+            int num = Integer.parseInt(br.readLine());
             // 추가
-            if(value > start){
-                for(int j=start+1; j<=value; j++){
+            if(num > start){
+                for(int j= start+1; j<=num; j++){
                     stack.push(j);
-                    sb.append("+").append("\n");
+                    sb.append('+').append("\n");
                 }
-                start =value;
+                start = num;
             }
 
-            // 오름차순 될 수 없는 경우
-            else if(stack.peek() != value){
+
+            // 크기 비교
+            else if(stack.peek() > num){
                 System.out.println("NO");
                 return;
             }
-
             // 제거
+
             stack.pop();
             sb.append("-").append("\n");
-
         }
 
         System.out.println(sb);
-
     }
 }
