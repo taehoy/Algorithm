cf0e2408d7700113307f1d2af1e129d75c54da30
Taeho Noh <124743189+devhoody@users.noreply.github.com>
2024-07-23 21:18:54 +0900

    [Gold V] Title: 탑, Time: 664 ms, Memory: 108224 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -5,32 +5,40 @@ import java.util.HashSet;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
+class Top{
+    int index;
+    int height;
+
+    Top(int num, int height){
+        this.index = num;
+        this.height = height;
+    }
+}
+
 public class Main {
     public static void main(String[] args) throws IOException {
-        Stack<int[]> stack = new Stack<>();
+        Stack<Top> stack = new Stack<>();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(br.readLine());
-
         StringBuilder sb = new StringBuilder();
 
-        // 스택 초기화
+        int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         for(int i=1; i<=N; i++){
             int height = Integer.parseInt(st.nextToken());
 
             while(!stack.isEmpty()){
-                if(stack.peek()[1] >= height){
-                    sb.append(stack.peek()[0]).append(" ");
+                if(stack.peek().height >= height){
+                    sb.append(stack.peek().index).append(" ");
                     break;
                 }
                 stack.pop();
             }
+
             if(stack.isEmpty()){
                 sb.append("0 ");
             }
-            stack.push(new int[]{i, height});
+            stack.push(new Top(i, height));
         }
 
         System.out.println(sb);
