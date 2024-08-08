719f525de59261284676b1b3e4ca9edd6a7947bf
Taeho Noh <124743189+taehoy@users.noreply.github.com>
2024-08-08 17:24:12 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.c
+++ 백준/Bronze/1008. A／B/A／B.c
@@ -5,23 +5,12 @@ int main(void) {
     char buffer[100];
     double num1, num2, result;
 
-    if (fgets(buffer, sizeof(buffer), stdin) != NULL) {
-        // 입력된 문자열을 두 개의 실수로 변환
-        if (sscanf(buffer, "%lf %lf", &num1, &num2) == 2) {
-            // 두 번째 수가 0이면 나눗셈 오류를 처리
-            if (num2 != 0) {
-                result = num1 / num2;
-                // 결과를 소수점 이하 9자리까지 출력
-                printf("%.9lf\n", result);
-            } else {
-                printf("Error: Division by zero is not allowed.\n");
-            }
-        } else {
-            printf("Invalid input. Please enter two numbers.\n");
-        }
-    } else {
-        printf("Error reading input.\n");
-    }
+    fgets(buffer, sizeof(buffer), stdin);
+    sscanf(buffer, "%lf %lf", &num1, &num2);
+
+    result = num1 / num2;
+
+    printf("%.9lf\n", result);
 
     return 0;
 }
