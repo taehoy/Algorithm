f88e5722c47b673b6e7f859cdc63da679ae84e08
Taeho Noh <124743189+taehoy@users.noreply.github.com>
2024-08-08 16:26:47 +0900

    [Bronze V] Title: A-B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/1001. A－B/A－B.c
+++ 백준/Bronze/1001. A－B/A－B.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 
 int main(){
-    int buffer[100];
     int num1, num2;
     
-    fgets(buffer, sizeof(buffer), stdin);
-    sscanf(buffer, "%d %d", &num1, &num2);
-    
-    printf("%d", num1 - num2);
+    scanf("%d %d", &num1, &num2);
+    printf("%d", num1-num2);
 }
