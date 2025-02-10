d1fd75997e9366ff6301e2b7223343ad58cf07ba
Taeho Noh <124743189+taehoy@users.noreply.github.com>
2025-02-10 10:29:29 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.py
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -1,12 +1,12 @@
 score = int(input())
 
-if 90 <= score <= 100 :
-    print('A')
-elif 80 <= score < 90 :
-    print('B')
-elif 70 <= score < 80 :
-    print('C')
-elif 60 <= score < 70 :
-    print('D')
-else :
+if score < 60 :
     print('F')
+elif score < 70 :
+    print('D')
+elif score < 80 :
+    print('C')
+elif score < 90 :
+    print('B')
+else :
+    print('A')
