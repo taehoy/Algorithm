0d48ffad201346ae2189ffd4e8def95dc0b7c506
Taeho Noh <124743189+taehoy@users.noreply.github.com>
2025-02-10 16:49:19 +0900

    [Silver III] Title: 두 수의 합, Time: 60 ms, Memory: 44956 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.py
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -1,25 +1,17 @@
 import sys
-
 input = sys.stdin.readline
+
+def solution(arr, x) :
+    numset = set(arr)
+    cnt = 0
+    for num in arr :
+        if x - num in numset :
+            cnt += 1
+    return cnt // 2
+
+
 n = int(input().strip())
 arr = list(map(int, input().split()))
 x = int(input().strip())
 
-arr.sort()
-
-left, right = 0, n-1
-count = 0 # 정답 개수
-
-while left < right :
-    total = arr[left] + arr[right]
-
-    if total == x :
-        count += 1
-        left += 1
-        right -= 1
-    elif total < x :
-        left += 1
-    else :
-        right -= 1
-
-print(count)
+print(solution(arr, x))
