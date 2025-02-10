696fb9d1482d27a488d2b35db30f73c9fbecdbc4
Taeho Noh <124743189+taehoy@users.noreply.github.com>
2025-02-10 17:26:17 +0900

    [Bronze II] Title: 문자열 반복, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.py
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -1,10 +1,8 @@
 T = int(input())
-result = ''
 for _ in range(T):
     R, S = input().split()
 
     R = int(R)
 
-    for char in S :
-        print(char * R, end="")
-    print()
+    result = "".join(char * R for char in S)
+    print(result)
