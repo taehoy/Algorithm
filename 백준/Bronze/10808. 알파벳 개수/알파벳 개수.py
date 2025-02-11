857349edec53f2b89a5e44e06a9e1e2a06abd531
Taeho Noh <124743189+taehoy@users.noreply.github.com>
2025-02-11 10:06:20 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
@@ -2,12 +2,10 @@
 # 단어에 맞는 인덱스 + 1
 # 리스트 출력 
 
-alphabets = [0] * 26
-
+result = [0] * 26
 S = input()
 
-for char in S :
-    alphabets[ord(char)-ord('a')] += 1
+for word in S :
+    result[ord(word) - ord('a')] += 1
 
-# 한줄로 출력 
-print(" ".join(map(str, alphabets)))
+print(" ".join(map(str, result)))
