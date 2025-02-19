4c8347f6d74539531e03166997945392164fb99e
Taeho Noh <124743189+taehoy@users.noreply.github.com>
2025-02-19 14:28:58 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -1,13 +1,17 @@
 import sys
 input = sys.stdin.readline
 
+n = int(input())
+
 def fibo(N) :
     if N == 0 :
         return 0
     if N == 1 :
         return 1
     
-    return fibo(N-1) + fibo(N-2)
+    a, b = 0 , 1
+    for _ in range(2, N+1):
+        a,b = b, a+b
+    return b
 
-n = int(input())
 print(fibo(n))
