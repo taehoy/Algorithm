7b23ad93ab8a29f2d0d2a8aeae1222d4f1e7d72f
Taeho Noh <124743189+taehoy@users.noreply.github.com>
2025-03-26 15:32:23 +0900

    [Bronze III] Title: 직각삼각형, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+arr = list(map(int, input().split()))
+
+while(True) :
+    if (arr[0] == 0 and arr[1] == 0 and arr[2] == 0) : 
+        break
+    
+    arr.sort()
+
+    if (arr[2] * arr[2]) == (arr[0] * arr[0]) + (arr[1] * arr[1]) :
+        print("right")
+    else :
+        print("wrong")
+    
+    arr= list(map(int, input().split()))
