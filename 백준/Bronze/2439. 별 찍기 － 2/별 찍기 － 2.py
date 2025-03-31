e2b506dd93d2fda839544e3db1ad04ee8a89bb58
Taeho Noh <124743189+taehoy@users.noreply.github.com>
2025-03-31 16:33:35 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.py
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.py
@@ -1,4 +1,8 @@
 n = int(input())
 
-for i in range(n):
-    print(' '*(n-(i+1)) + '*'*(i+1))
+for i in range(n) :
+    for j in range(n - (i+1)) :
+        print(" ", end="")
+    for j in range(i+1):
+        print('*', end="")
+    print()
