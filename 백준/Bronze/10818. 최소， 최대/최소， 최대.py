bb67ac71fd5f12a03fe714a4d9b2327e2cd524b1
Taeho Noh <124743189+taehoy@users.noreply.github.com>
2025-03-31 19:05:12 +0900

    [Bronze III] Title: 최소, 최대, Time: 312 ms, Memory: 129196 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.py
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -1,8 +1,5 @@
 n = int(input())
 arr = list(map(int, input().split()))
-# 정렬한다.
-arr.sort()
 # 최대,최소를 구한다.
-## 최대 : 마지막(n-1) 인덱스요소
-## 최소 : 인덱스 0 요소 
-print(arr[0], arr[n-1])
+## max(), min() 사용
+print(min(arr), max(arr))
