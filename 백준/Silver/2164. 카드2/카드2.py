76d5738f84742c7a4005203ac3feb48a24a8737d
Taeho Noh <124743189+taehoy@users.noreply.github.com>
2025-04-02 10:27:32 +0900

    [Silver IV] Title: 카드2, Time: 200 ms, Memory: 51888 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -0,0 +1,14 @@
+import sys
+from collections import deque
+
+n = int(input())
+que = deque()
+
+for i in range(1, n+1) :
+    que.append(i)
+
+while len(que) > 1 :
+    que.popleft()
+    que.append(que.popleft())
+
+print(que[0])
