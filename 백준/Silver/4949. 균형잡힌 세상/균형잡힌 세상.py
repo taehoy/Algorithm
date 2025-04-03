0994933d4504c5d0dfd1c2c5b236a4584d971dc0
Taeho Noh <124743189+taehoy@users.noreply.github.com>
2025-04-03 19:05:51 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 232 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -1,28 +1,26 @@
-while True:
-    a = input()
+while(True) :
+    st = input()
     stack = []
 
-    if a == ".":
+    if st == '.' :
         break
 
-    for i in a :
-        if i == '[' or i == '(' :
-            stack.append(i)
-        
-        elif i == ']' :
-            if len(stack) != 0 and stack[-1] == '[' :
-                stack.pop()
-                
-            else :
-                stack.append(']')
-                break
-        elif i == ')' :
+    for s in st :
+        if s == '(' or s == '[' :
+            stack.append(s)
+        elif s == ')' :
             if len(stack) != 0 and stack[-1] == '(' :
                 stack.pop()
             else :
-                stack.append(')')
+                stack.append(s)
                 break
-    
+        elif s == ']' :
+            if len(stack) != 0 and stack[-1] == '[' :
+                stack.pop()
+            else :
+                stack.append(s)
+                break
+
     if len(stack) == 0 :
         print("yes")
     else :
