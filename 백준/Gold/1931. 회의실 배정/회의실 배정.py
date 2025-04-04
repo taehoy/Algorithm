a456c472e1cef275e72f86b8deb52fd18bbe88ee
Taeho Noh <124743189+taehoy@users.noreply.github.com>
2025-04-04 14:30:20 +0900

    [Gold V] Title: 회의실 배정, Time: 244 ms, Memory: 60360 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.py
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -2,15 +2,15 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-meets = [list(map(int,input().split())) for _ in range(n)]
+endTime = 0
+answer = 0 
+
+meets = [list(map(int, input().split())) for _ in range(n)]
 
 meets.sort(key=lambda x:(x[1],x[0]))
-
-answer = 0
-endTime = 0
 
 for i in range(len(meets)) :
     if endTime <= meets[i][0] :
         endTime = meets[i][1]
         answer += 1
-print(answer )
+print(answer)
