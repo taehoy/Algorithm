03d479b661f65614e432a86c05c92366b10e073a
Taeho Noh <124743189+taehoy@users.noreply.github.com>
2025-04-04 14:54:48 +0900

    [Bronze III] Title: 팩토리얼, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.py
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.py
@@ -1,10 +1,9 @@
-def factorial(n):
-    if n == 0:
+n= int(input())
+
+def factorial(n) :
+    if n == 1 or n == 0 :
         return 1
     
-    return n * factorial(n-1)
-
-n = int(input())
+    return n * factorial(n-1) 
 
 print(factorial(n))
-
