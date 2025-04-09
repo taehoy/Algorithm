161d40115443c00306293ca2efd85322c23f4f1b
Taeho Noh <124743189+taehoy@users.noreply.github.com>
2025-04-09 16:48:17 +0900

    [Silver III] Title: 01타일, Time: 236 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.py
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -1,11 +1,13 @@
 n = int(input())
 
-dp = [0] * (n+2)
+if n == 1 :
+    print(1)
+elif n == 2 :
+    print(2)
+else :
+    a, b = 1, 2
 
-dp[1] = 1
-dp[2] = 2
+    for _ in range(3, n+1) :
+        a, b = b, (a+b) % 15746
 
-for i in range(3, n+1) :
-    dp[i] = (dp[i-1] + dp[i-2]) % 15746
-
-print(dp[n])
+    print(b)
