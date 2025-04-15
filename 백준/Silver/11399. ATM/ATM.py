234d868b99a5cc110c4d927812ab53849afe57fb
Taeho Noh <rethonly@gmail.com>
2025-04-15 11:38:49 +0900

    [Silver IV] Title: ATM, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,12 +1,8 @@
+from itertools import accumulate
+
 n = int(input())
 
 arr = list(map(int, input().split()))
 arr.sort()
 
-result = 0
-answer = 0
-for num in arr :
-    result += num
-    answer += result
-
-print(answer)
+print(sum(accumulate(arr)))
