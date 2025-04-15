2c8a6b2536cb95d38e9067a22789df99e6b00468
Taeho Noh <rethonly@gmail.com>
2025-04-15 13:27:34 +0900

    [Silver IV] Title: 수 찾기, Time: 172 ms, Memory: 53112 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+arr = list(map(int, input().split()))
+
+m = int(input())
+arr2 = list(map(int, input().split()))
+
+dic = {}
+for num in arr :
+    dic[num] = 1
+
+for num in arr2 :
+    if num in dic.keys():
+        print(1)
+    else : 
+        print(0)
