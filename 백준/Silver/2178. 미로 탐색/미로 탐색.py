9505d723aa177c297cdd447a3d64b8d914597a54
Taeho Noh <rethonly@gmail.com>
2025-04-19 14:42:27 +0900

    [Silver I] Title: 미로 탐색, Time: 60 ms, Memory: 34072 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,31 +1,34 @@
+import sys
 from collections import deque
+input = sys.stdin.readline
+sys.setrecursionlimit(10**8) 
 
-n, m = map(int, input().split())
-board = [list(map(int, input())) for _ in range(n)]
-
-dx = [1, 0, -1, 0]
+# 상하좌우 확인시 필요한 리스트
 dy = [0, 1, 0, -1]
+dx = [1, 0, -1, 0] 
 
-def BFS(x, y):
-    q = deque()
-    q.append([x,y])
+N, M = map(int, input().split())
+
+graph = []
+
+for i in range(N):
+    graph.append(list(map(int, input().rstrip())))
+
+def bfs(x, y):
+    q = deque([(x,y)]) # 큐 초기화시 값 넣기(리스트)
 
     while q :
-        x,y = q.popleft()
+        x, y = q.popleft() 
 
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+            nx = x + dx[i] # 좌우 확인
+            ny = y + dy[i] # 상하 확인
 
-            if nx < 0 or nx >= n or ny < 0 or ny >= m:
-                continue
+            # 그래프 범위 벗어나면 패스
+            if 0 <= nx < M and 0 <= ny < N and graph[ny][nx] ==1 :
+                graph[ny][nx] = graph[y][x]+1
+                q.append((nx,ny))
 
-            if board[nx][ny] == 0 : continue
-            if board[nx][ny] == 1 :
-                board[nx][ny] = board[x][y]+1
-                q.append([nx, ny])
-        
-    return board[n-1][m-1]
+    return graph[N-1][M-1]
 
-print(BFS(0,0))
-
+print(bfs(0,0))
