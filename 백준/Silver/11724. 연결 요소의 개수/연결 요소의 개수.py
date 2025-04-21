532f2ced4093c7dfeebb2d20eddc463713f10a7e
Taeho Noh <rethonly@gmail.com>
2025-04-21 14:57:51 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 560 ms, Memory: 39196 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -0,0 +1,42 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**8) 
+
+
+N, M = map(int, input().split())
+count = 0
+
+graph = [[False] * (N+1) for _ in range(N+1)]
+visited = [False] * (N+1)
+
+for i in range(M):
+    a, b = map(int, input().split())
+
+    # # bfs에서 처음 시작 큐 설정 
+    # if i == 0 :
+    #     q.append(a)
+
+    graph[a][b] = True
+    graph[b][a] = True
+
+def bfs():
+    global q, visited
+
+    while q :
+        cur = q.pop()
+        visited[cur] = True
+
+        for next in range(1, N+1):
+            if not visited[next] and graph[cur][next] :
+                visited[next] = True
+                q.append(next)
+
+q = []
+
+for i in range(1, N+1):
+    if not visited[i] :
+        count += 1
+        q.append(i)
+        bfs()
+
+print(count)
