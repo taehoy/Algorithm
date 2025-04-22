cdd27bf791d1a4de917c68192a5962dc177d7645
Taeho Noh <rethonly@gmail.com>
2025-04-22 11:20:05 +0900

    [Silver I] Title: 나이트의 이동, Time: 1092 ms, Memory: 34984 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -3,26 +3,26 @@ from collections import deque
 input = sys.stdin.readline
 
 def bfs(y, x) :
-    
+    visited = [[-1] * l for _ in range(l)]
     q = deque([(y, x)])
+    visited[y][x] = 0
 
     while q :
         y, x = q.popleft()
 
         if (y, x) == (target_y, target_x) :
-            return graph[y][x]
+            return visited[y][x]
 
         for dy, dx in directions :
             ny = y + dy
             nx = x + dx
 
             if 0 <= ny < l and 0 <= nx < l :
-                if graph[ny][nx] == 0 :
-                    graph[ny][nx] = graph[y][x] + 1
+                if visited[ny][nx] == -1 :
+                    visited[ny][nx] = visited[y][x] + 1
                     q.append((ny,nx))
 
     return "갈수없음"
-
 
 T = int(input())
 
@@ -32,7 +32,6 @@ for _ in range(T):
     # 입력 및 초기화
     l = int(input())
     cy, cx = map(int,input().split())
-    graph = [[0] * (l) for _ in range(l)]
     target_y, target_x = map(int, input().split())
 
     # bfs
