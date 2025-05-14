03fe08a22d91752812c4e8b5c623f2cd323cee6b
Taeho Noh <rethonly@gmail.com>
2025-05-14 14:48:21 +0900

    [Bronze II] Title: 숫자의 개수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -1,12 +1,8 @@
-arr = [0] * 10
+a = 1
+for _ in range(3):
+    a *= int(input())
 
-a = int(input())
-b = int(input())
-c = int(input())
+a = str(a)
 
-total = a * b * c
-
-for i in str(total):
-    arr[int(i)] += 1
-
-print('\n'.join(map(str, arr)))
+for i in range(10):
+    print(a.count(str(i)))
