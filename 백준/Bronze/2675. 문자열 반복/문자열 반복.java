67e63c392783648a2cd4f401701fdf903bc6a70a
Taeho Noh <rethonly@gmail.com>
2025-10-27 22:54:03 +0900

    [Bronze II] Title: 문자열 반복, Time: 100 ms, Memory: 14048 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -5,25 +5,29 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
 
-        int N = Integer.parseInt(br.readLine());
-
-        for(int i=0; i<N; i++){
+        for(int i = 0; i<T; i++){
             StringBuilder sb = new StringBuilder();
 
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            int n = Integer.parseInt(st.nextToken());
-            String s = st.nextToken();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int num = Integer.parseInt(st.nextToken());
+            String word = st.nextToken();
 
-            for(int j=0; j<s.length(); j++){
-                char c = s.charAt(j);
+            for (int j = 0; j < word.length(); j++) {
+                char c = word.charAt(j);
 
-                for(int k=0; k<n; k++){
-                    sb.append(c);
-                }
+                for(int k=0; k<num; k++) sb.append(c);
+
             }
+
             System.out.println(sb);
         }
+
+        br.close();
+
+
     }
 }
