dceffeda8e71fef113c3815d4a5efe924b99a534
Taeho Noh <rethonly@gmail.com>
2025-10-27 22:12:52 +0900

    [Bronze V] Title: 구구단, Time: 200 ms, Memory: 18516 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,23 +1,12 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.util.Scanner;
 
 public class Main{
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
         
-        int N = Integer.parseInt(br.readLine());
-        
-        for(int i=1; i<10; i++) {
-            bw.write(N + " * " + i + " = " + N*i + "\n");
+        for (int i = 1; i<=9; i++) {
+            System.out.println(num +" * "+ i + " = " + num*i);
         }
-        
-        br.close();
-        bw.flush();
-        bw.close();
-        
     }
 }
