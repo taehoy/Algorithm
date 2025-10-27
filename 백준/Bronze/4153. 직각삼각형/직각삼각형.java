e0e17a44e62918a7ffe47660c0d4f5b7dfa3d6ab
Taeho Noh <rethonly@gmail.com>
2025-10-28 00:24:10 +0900

    [Bronze III] Title: 직각삼각형, Time: 104 ms, Memory: 14016 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.java
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -8,22 +9,26 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        while(true){
+        while (true) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            int n1 = Integer.parseInt(st.nextToken());
-            int n2 = Integer.parseInt(st.nextToken());
-            int n3 = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
 
-            // 0 0 0 이면 종료
-            if(n1 == 0 && n2 == 0 && n3 == 0) break;
+            if(a == 0 && b == 0 && c == 0) break;
 
-            if((n1 * n1 + n2 * n2) == n3 * n3) sb.append("right").append("\n");
-            else if(n1 * n1 == (n2 * n2 + n3 * n3)) sb.append("right").append("\n");
-            else if((n1 * n1 + n3 * n3) == n2 * n2) sb.append("right").append("\n");
-            else sb.append("wrong").append("\n");
+            int aPow = (int) Math.pow(a, 2);
+            int bPow = (int) Math.pow(b, 2);
+            int cPow = (int) Math.pow(c, 2);
 
+            if (cPow == (aPow + bPow)) sb.append("right").append("\n");
+            else if ((cPow +aPow) == bPow) sb.append("right").append("\n");
+            else if (aPow == (cPow + bPow)) sb.append("right").append("\n");
+            else{
+                sb.append("wrong").append("\n");
+            }
         }
-        
+
         System.out.println(sb);
         br.close();
     }
