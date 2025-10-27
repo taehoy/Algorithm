bf9178b9ea70e1c26264f754525391c4cebed387
Taeho Noh <rethonly@gmail.com>
2025-10-27 22:10:15 +0900

    [Bronze V] Title: 시험 성적, Time: 172 ms, Memory: 17692 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -1,23 +1,14 @@
 import java.util.Scanner;
 
 public class Main{
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        int score = sc.nextInt();
         
-        int a = sc.nextInt();
-        
-        sc.close();
-        
-        if(90<=a && a<=100)
-            System.out.println('A');
-        else if(80<=a && a<=89)
-            System.out.println('B');
-        else if(70<=a && a<=79)
-            System.out.println('C');
-        else if(60<=a && a<=69)
-            System.out.println('D');
-        else 
-            System.out.println('F');
-
+        if(90 <= score && score <= 100) System.out.println('A');
+        else if(80 <= score && score < 90) System.out.println('B');
+        else if(70 <= score && score < 80) System.out.println('C');
+        else if(60 <= score && score < 70) System.out.println('D');
+        else System.out.println("F");
     }
 }
