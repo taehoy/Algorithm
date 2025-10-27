15d4848fe0257c457855439d14e9f8a58363080f
Taeho Noh <rethonly@gmail.com>
2025-10-28 00:48:06 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 668 ms, Memory: 94556 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,3 +1,5 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -5,24 +7,28 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        // 두배의 길이 배열 만들기
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int N = Integer.parseInt(br.readLine());
+        int num = Integer.parseInt(br.readLine());
 
-        int[] arr = new int[N];
+        boolean[] arr = new boolean[2000001];
 
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+        for (int i = 0; i < num; i++) {
+            // 입력한 값의 +1000000 의 배열에 표시
+            int num1 = Integer.parseInt(br.readLine());
+            arr[num1+1000000] = true;
         }
 
-        Arrays.sort(arr);
-
-        for (int i = 0; i < N; i++) {
-            sb.append(arr[i]).append("\n");
+        // 배열 내 표시한 수의 -1000000을 출력한다.
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]) {
+                sb.append(i - 1000000).append("\n");
+            }
         }
 
         System.out.println(sb);
-
         br.close();
+
     }
 }
