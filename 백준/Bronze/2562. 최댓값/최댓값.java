84e678bdaf8a3f8d239463663187cd1d23199ff6
Taeho Noh <rethonly@gmail.com>
2025-10-27 22:31:13 +0900

    [Bronze III] Title: 최댓값, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -3,20 +3,18 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main{
-    public static void main(String[] args) throws IOException {
-        
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        int max =0;
+        int max = 0;
         int index = 0;
         
-        for(int i=0; i<9; i++) {
-            
+        for(int i=0; i<9; i++){
             int value = Integer.parseInt(br.readLine());
             
-            if(value>max) {
+            if(value > max){
                 max = value;
-                index = i+1;
+                index = i + 1;
             }
         }
         
