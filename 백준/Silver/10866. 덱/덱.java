548eb02f5fee6a0f28723cdc53c8a665645f46e0
Taeho Noh <rethonly@gmail.com>
2025-10-28 20:48:42 +0900

    [Silver IV] Title: 덱, Time: 160 ms, Memory: 18752 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.java
+++ 백준/Silver/10866. 덱/덱.java
@@ -1,51 +1,52 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
 
-        Deque<Integer> dq = new LinkedList<>();
+        // 덱생성
+        Deque<Integer> deque = new ArrayDeque<>();
 
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
+        for (int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            String cmd = st.nextToken();
 
-        for(int i=0; i<N;i++){
-            st = new StringTokenizer(br.readLine());
-
-            String s = st.nextToken();
-
-            switch (s){
-                case "push_front" :
+            switch (cmd) {
+                case "push_back":
                     int num = Integer.parseInt(st.nextToken());
-                    dq.addFirst(num);
+                    deque.offerLast(num);
                     break;
-                case "push_back" :
+                case "push_front":
                     int num1 = Integer.parseInt(st.nextToken());
-                    dq.add(num1);
+                    deque.offerFirst(num1);
                     break;
-                case "pop_front" :
-                    sb.append(dq.isEmpty() ? -1 : dq.poll()).append("\n");
+                case "pop_front":
+                    sb.append(deque.isEmpty() ? -1 : deque.pollFirst()).append("\n");
                     break;
-                case "pop_back" :
-                    sb.append(dq.isEmpty() ? -1 : dq.pollLast()).append("\n");
+                case "pop_back":
+                    sb.append(deque.isEmpty() ? -1 : deque.pollLast()).append("\n");
                     break;
-                case "size" :
-                    sb.append(dq.size()).append("\n");
+                case "size":
+                    sb.append(deque.size()).append("\n");
                     break;
-                case "empty" :
-                    sb.append(dq.isEmpty() ? 1 : 0).append("\n");
+                case "empty":
+                    sb.append(deque.isEmpty() ? 1 : 0).append("\n");
                     break;
-                case "front" :
-                    sb.append(dq.isEmpty() ? -1 : dq.peek()).append("\n");
+                case "front":
+                    sb.append(deque.isEmpty() ? -1 : deque.peekFirst()).append("\n");
                     break;
-                case "back" :
-                    sb.append(dq.isEmpty() ? -1 : dq.peekLast()).append("\n");
+                case "back":
+                    sb.append(deque.isEmpty() ? -1 : deque.peekLast()).append("\n");
                     break;
             }
         }
