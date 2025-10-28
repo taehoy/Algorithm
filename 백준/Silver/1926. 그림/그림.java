9f6dc07b0cb26d78e4b6c441a78526f7df016378
Taeho Noh <rethonly@gmail.com>
2025-10-28 21:29:21 +0900

    [Silver I] Title: 그림, Time: 392 ms, Memory: 50260 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -1,80 +1,92 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.StringTokenizer;
+
+class Node {
+    int x, y;
+    Node(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
 
 public class Main {
-    static class Node {
-        int x;
-        int y;
-
-        Node(int n, int m){
-            x = n;
-            y = m;
-        }
-    }
-
+    static int N;
+    static int M;
     static int[][] board;
-    static boolean[][] vis;
+    static boolean[][] visited;
 
-    static int n;
-    static int m;
-    static int[] dx = new int[]{1, 0, -1, 0};
-    static int[] dy = new int[]{0, 1, 0, -1};
+    static int max;
+    static int cnt;
+    // 상하좌우
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] arr = br.readLine().split(" ");
-        n = Integer.parseInt(arr[0]);
-        m = Integer.parseInt(arr[1]);
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        board = new int[n][m];
-        vis = new boolean[n][m];
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        for(int i=0; i<n; i++){
+        // 방문여부, 보드판
+        visited = new boolean[N][M];
+        board = new int[N][M];
+
+        // 보드 생성
+        for (int i = 0; i < N; i++) {
             String[] strings = br.readLine().split(" ");
-            for(int j=0; j<m; j++){
+            for (int j = 0; j < M; j++) {
                 board[i][j] = Integer.parseInt(strings[j]);
             }
         }
 
-        int mx = 0; // 그림의 최댓값
-        int num = 0; // 그림의 수
+        max = 0; // 가장 큰 넓이
+        cnt = 0; // 개수
 
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(board[i][j] == 0 || vis[i][j]) continue;
-
-                num++; // 그림 개수 +1
-                Queue<Node> q = new LinkedList<>();
-
-                vis[i][j] = true;
-                q.offer(new Node(i,j));
-
-                int area = 0; // 그림 크기
-
-                while(!q.isEmpty()){
-                    area++; // 크기 +1
-                    Node curr = q.poll();
-
-                    for(int k =0; k<4; k++){
-                        int nx = curr.x + dx[k];
-                        int ny = curr.y + dy[k];
-
-                        if(nx <0 || nx >=n || ny <0 || ny >= m) continue;
-                        if(vis[nx][ny] || board[nx][ny] != 1) continue;
-                        vis[nx][ny] = true;
-                        q.offer(new Node(nx,ny));
-                    }
+        //넓이
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (!visited[i][j] && board[i][j] == 1){
+                    cnt++;
+                    max = Math.max(max, bfs(new Node(i, j)));
                 }
-
-                mx = Math.max(mx, area);
-
             }
         }
-        System.out.println(num);
-        System.out.println(mx);
+
+        System.out.println(cnt);
+        System.out.println(max);
     }
 
+    private static int bfs(Node node) {
+        Queue<Node> q = new LinkedList<>();
+        q.add(node);
+        visited[node.x][node.y] = true;
+        int result = 0;
+
+        while (!q.isEmpty()) {
+            Node poll = q.poll();
+            int cx = poll.x;
+            int cy = poll.y;
+
+            if(board[cx][cy] == 1) result++;
+
+            for (int i = 0; i < 4; i++) {
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
+
+                if(nx <0 || nx >= N || ny < 0 || ny >= M) continue;
+                if(visited[nx][ny] || board[nx][ny] == 0) continue;
+                visited[nx][ny] = true;
+                q.add(new Node(nx, ny));
+            }
+        }
+
+        return result;
+    }
 }
