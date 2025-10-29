5ded76877d592a45a00f06025b255966b8444efc
Taeho Noh <rethonly@gmail.com>
2025-10-29 21:26:38 +0900

    [Silver V] Title: 나이순 정렬, Time: 368 ms, Memory: 41564 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,6 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -10,22 +9,22 @@ public class Main {
 
         StringBuilder[] arr = new StringBuilder[201];
 
+        StringTokenizer st;
         for(int i=0; i<arr.length; i++){
             arr[i] = new StringBuilder();
         }
-        StringTokenizer st;
 
-        for(int i=0; i<N; i++){
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             int age = Integer.parseInt(st.nextToken());
             String name = st.nextToken();
 
-            arr[age].append(age).append(' ').append(name).append("\n");
+            arr[age].append(age).append(" ").append(name).append("\n");
         }
 
         StringBuilder sb = new StringBuilder();
 
-        for(StringBuilder p : arr){
+        for (StringBuilder p : arr) {
             sb.append(p);
         }
 
