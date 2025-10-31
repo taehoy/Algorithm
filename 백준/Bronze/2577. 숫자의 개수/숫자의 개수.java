8170630972b576a402c405a318dfae7ad3fad851
Taeho Noh <rethonly@gmail.com>
2025-10-31 20:09:10 +0900

    [Bronze II] Title: 숫자의 개수, Time: 100 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -1,22 +1,27 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int num1 = Integer.parseInt(br.readLine());
-        int num2 = Integer.parseInt(br.readLine());
-        int num3 = Integer.parseInt(br.readLine());
+        int[] arr1 = new int[3];
+        for (int i = 0; i < 3; i++) {
+            arr1[i] = Integer.parseInt(br.readLine());
+        }
 
-        int result = num1* num2 * num3;
+        int result = arr1[0] * arr1[1] * arr1[2];
 
         int[] arr = new int[10];
 
         while(result != 0){
-            arr[result % 10]++;
+            int num = result % 10;
             result /= 10;
+            arr[num]++;
+
         }
 
         StringBuilder sb = new StringBuilder();
@@ -25,5 +30,6 @@ public class Main {
         }
 
         System.out.println(sb);
+
     }
 }
