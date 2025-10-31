2bf410af5dc3912c889321ccd1e452dc1daed486
Taeho Noh <rethonly@gmail.com>
2025-10-31 20:24:06 +0900

    [Silver V] Title: 방 번호, Time: 96 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.java
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -1,28 +1,31 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+public class Main{
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-        String s = br.readLine();
-
+        // 카드 넣을 배열 생성
         int[] arr = new int[10];
+        // 9인경우 6에 ++
+        while(n != 0){
+            if(n % 10 == 9) arr[6]++;
+            else arr[n % 10]++;
+            n /= 10;
 
-        for(char c : s.toCharArray()){
-            if(c-'0' == 9){
-                arr[6]++;
-            } else{
-                arr[c-'0']++;
-            }
         }
 
-        arr[6] = (int)Math.ceil((double)arr[6]/2);
+        arr[6] = (int)Math.ceil((double)arr[6] / 2);
 
+        // 배열 돌면서 가장 큰 개수가 카드의 개수
         int max = 0;
-        for(int i : arr){
-            max = Math.max(max, i);
+
+
+        for(int i : arr) {
+            if (max < i) {
+                max = i;
+            }
         }
 
         System.out.println(max);
