91693c94574b92b818b1b4dfde7bf461d300d106
Taeho Noh <rethonly@gmail.com>
2025-11-01 22:03:42 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 952 ms, Memory: 58064 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -1,49 +1,41 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        String[][] arr = new String[n][2];
 
-        int N =Integer.parseInt(br.readLine());
-
-        String[][] arr = new String[N][2];
-
-        StringTokenizer st;
-
-        for(int i=0; i<N; i++){
-            st = new StringTokenizer(br.readLine(), " ");
-            arr[i][0] = st.nextToken();
-            arr[i][1] = st.nextToken();
+        for (int i = 0; i < n; i++) {
+            String[] strings = br.readLine().split(" ");
+            arr[i][0] = strings[0];
+            arr[i][1] = strings[1];
         }
 
-        Arrays.sort(arr, new Comparator<String[]>() {
-            @Override
+        Arrays.sort(arr, new Comparator<String[]>(){
             public int compare(String[] o1, String[] o2) {
-                int x1 = Integer.parseInt(o1[0]);
-                int x2 = Integer.parseInt(o2[0]);
+                int num1 = Integer.parseInt(o1[0]);
+                int num2 = Integer.parseInt(o2[0]);
 
-                int result = x1 - x2;
+                if(num1 == num2){
+                    int num3 = Integer.parseInt(o1[1]);
+                    int num4 = Integer.parseInt(o2[1]);
 
-                if(result == 0){
-                    int y1 = Integer.parseInt(o1[1]);
-                    int y2 = Integer.parseInt(o2[1]);
-
-                    return y1 - y2;
+                    return num3 - num4;
+                } else {
+                    return num1 - num2;
                 }
-
-                return result;
             }
         });
 
-        StringBuilder sb = new StringBuilder();
-
-        for(int i=0; i<N; i++){
-            sb.append(arr[i][0]).append(' ').append(arr[i][1]).append("\n");
+        for (int i = 0; i < n; i++) {
+            sb.append(arr[i][0]).append(" ").append(arr[i][1]).append("\n");
         }
 
         System.out.println(sb);
