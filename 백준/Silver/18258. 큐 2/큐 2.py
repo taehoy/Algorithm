3e393b9c027aaf79cee1d85f5d0512c11d35f695
Taeho Noh <rethonly@gmail.com>
2025-11-01 21:00:20 +0900

    [Silver IV] Title: 큐 2, Time: 1620 ms, Memory: 110608 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.py
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -1,34 +1,38 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 
-que = deque()
-n = int(input())
+from collections import deque
 
-for _ in range(n) :
+N = int(input())
+
+queue = deque()
+
+for _ in range(N):
     cmd = input().split()
 
-    if cmd[0] == "push" :
-        que.append(cmd[1])
-    elif cmd[0] == "pop" :
-        if len(que) == 0 :
-            print(-1)
+    if cmd[0] == "push":
+        num = int(cmd[1])
+        queue.append(num)
+    elif cmd[0] == "front":
+        if len(queue) == 0 :
+            print("-1")
         else :
-            print(que.popleft())
+            print(queue[0])
+    elif cmd[0] == "back":
+        if len(queue) == 0 :
+            print("-1")
+        else :
+            print(queue[-1])
     elif cmd[0] == "size" :
-        print(len(que))
+        print(len(queue))
     elif cmd[0] == "empty" :
-        if len(que) == 0 :
+        if len(queue) == 0:
             print(1)
         else :
             print(0)
-    elif cmd[0] == "front" :
-        if len(que) == 0 :
+    elif cmd[0] == "pop" :
+        if len(queue) == 0:
             print(-1)
         else :
-            print(que[0])
-    elif cmd[0] == "back" :
-        if len(que) == 0 :
-            print(-1)
-        else :
-            print(que[-1])
+            print(queue.popleft())
+
