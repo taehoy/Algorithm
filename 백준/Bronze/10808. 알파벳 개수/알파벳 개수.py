8ca23d53a4022aa4f10c19001e67d62dc8f3264c
Taeho Noh <rethonly@gmail.com>
2025-11-14 21:59:42 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
@@ -1,11 +1,8 @@
-# a-z 리스트 생성
-# 단어에 맞는 인덱스 + 1
-# 리스트 출력 
+s = input()
 
-result = [0] * 26
-S = input()
+arr = [0] * 26
 
-for word in S :
-    result[ord(word) - ord('a')] += 1
+for c in s :
+    arr[ord(c) - ord('a')] += 1
 
-print(*result)
+print(*arr)
