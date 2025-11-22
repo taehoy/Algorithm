797a985718c07422285b4d6882a8ed6d9883b426
Taeho Noh <rethonly@gmail.com>
2025-11-22 15:51:20 +0900

    [Silver IV] Title: 스택, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,28 +1,32 @@
 import sys
 input = sys.stdin.readline
 
+s = []
+
 n = int(input())
-arr = []
 
 for _ in range(n):
     cmd = input().split()
-
-    if cmd[0] == "push" :
-        arr.append(cmd[1])
-    elif cmd[0] == "top" :
-        if len(arr) == 0 :
+    
+    if cmd[0] == "push":
+        s.append(cmd[1])
+    elif cmd[0] == "top":
+        if len(s) == 0 :
             print(-1)
         else :
-            print(arr[-1])
-    elif cmd[0] == "size" :
-        print(len(arr))
-    elif cmd[0] == "pop" :
-        if len(arr) == 0 :
+            print(s[-1])
+    elif cmd[0] == "size":
+        print(len(s))
+    elif cmd[0] == "pop":
+        if len(s) == 0 :
             print(-1)
         else :
-            print(arr.pop())
+            print(s.pop())
     elif cmd[0] == "empty":
-        if len(arr) == 0:
+        if len(s) == 0 :
             print(1)
         else :
             print(0)
+
+    
+    
