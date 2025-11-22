048bb7deabcf56c7901858357e907f74f73f74ea
Taeho Noh <rethonly@gmail.com>
2025-11-22 15:56:49 +0900

    [Silver IV] Title: 제로, Time: 2416 ms, Memory: 33192 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.py
+++ 백준/Silver/10773. 제로/제로.py
@@ -1,12 +1,11 @@
 n = int(input())
-
-stack = []
-for _ in range(n) :
+arr = []
+for _ in range(n):
     num = int(input())
-
+    
     if num == 0 :
-        stack.pop()
+        arr.pop()
     else :
-        stack.append(num)
+        arr.append(num)
 
-print(sum(stack))
+print(sum(arr))
