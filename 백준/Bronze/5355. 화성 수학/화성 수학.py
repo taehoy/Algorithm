e68620986232630dfb0080c6dd5d88c62fcdeee0
Taeho Noh <rethonly@gmail.com>
2026-03-21 15:35:22 +0900

    [Bronze II] Title: 화성 수학, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5355. 화성 수학/화성 수학.py
+++ 백준/Bronze/5355. 화성 수학/화성 수학.py
@@ -3,15 +3,13 @@ T = int(input())
 for _ in range(T):
     arr = input().split()
     
-    num = 0.0
-    for s in arr :
+    num = float(arr[0])
+    for s in arr[1:] :
         if(s == '@'):
             num *= 3
         elif(s == '%'):
             num += 5
         elif(s == '#'):
             num -= 7
-        else :
-            num = float(s)
         
     print(f"{num:.2f}")
