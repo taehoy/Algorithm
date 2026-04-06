c3eb3aa81dfa64870f562b8bfa23df60cab7233d
Taeho Noh <rethonly@gmail.com>
2026-04-06 12:45:55 +0900

    [Bronze III] Title: 주사위 게임, Time: 88 ms, Memory: 34924 KB -BaekjoonHub

--- 백준/Bronze/2476. 주사위 게임/주사위 게임.py
+++ 백준/Bronze/2476. 주사위 게임/주사위 게임.py
@@ -1,23 +1,23 @@
+from collections import Counter
+
 N = int(input())
-result = 0 
+result = 0
 
 for _ in range(N):
     dice = list(map(int, input().split()))
+    counter = Counter(dice)
 
-    dice_set = set(dice)
-
-    if len(dice_set) == 1 :
-        temp = 10000 + dice[0] * 1000
+    if len(counter) == 1:
+        num = dice[0]
+        temp = 10000 + num * 1000
     
-    elif len(dice_set) == 2 :
-        for num in dice_set :
-            if dice.count(num) == 2 :
+    elif len(counter) == 2 :
+        for num, cnt in counter.items():
+            if cnt == 2 :
                 temp = 1000 + num * 100
-                break
-    elif len(dice_set) == 3:
+                
+    else :
         temp = max(dice) * 100
     
     result = max(result, temp)
-
-
 print(result)
