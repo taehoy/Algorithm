deac2a73eed9c6fbea5ef7fc059f631f286dee9e
Taeho Noh <rethonly@gmail.com>
2026-04-10 12:39:50 +0900

    [Bronze III] Title: TGN, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5063. TGN/TGN.py
+++ 백준/Bronze/5063. TGN/TGN.py
@@ -1,10 +1,13 @@
 N = int(input())
+result = []
 
 for _ in range(N):
     r, e, c = map(int,input().split())
 
     cal = e - c
 
-    if r > cal : print("do not advertise")
-    elif r < cal : print("advertise")
-    else : print("does not matter")
+    if r > cal : result.append("do not advertise")
+    elif r < cal : result.append("advertise")
+    else : result.append("does not matter")
+
+print("\n".join(result))
