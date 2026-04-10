c22745ca8a3b23300a95ba61d5e2f3b1e0d43649
Taeho Noh <rethonly@gmail.com>
2026-04-10 12:44:51 +0900

    [Bronze III] Title: 개표, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10102. 개표/개표.py
+++ 백준/Bronze/10102. 개표/개표.py
@@ -1,16 +1,9 @@
 V = int(input())
 s = input()
 
-cntA = 0
-cntB = 0
+cntA = s.count('A')
+cntB = V - cntA
 
-for c in s:
-    if c == 'A': cntA +=1
-    else : cntB +=1
-
-if cntA > cntB :
-    print('A')
-elif cntA < cntB :
-    print('B')
-else :
-    print("Tie")
+if cntA > cntB : print('A')
+elif cntA < cntB : print('B')
+else : print('Tie')
