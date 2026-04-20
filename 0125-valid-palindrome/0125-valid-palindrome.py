c9daf87ee5fbecdd09fffeae479f883d95d70b6a
Taeho Noh <rethonly@gmail.com>
2026-04-20 13:25:32 +0900

    Time: 11 ms (33.47%), Space: 20.1 MB (33.11%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        replace_cmd = ''.join(c for c in s if c.isalpha() or c.isdigit())
+
+        # 빈 공백인 경우
+        if replace_cmd == '' :
+            return True
+        
+        # 양쪽이 일치하는 경우
+        upper_cmd = replace_cmd.upper()
+        upper_cmd_length = len(upper_cmd) // 2
+
+        for i in range(upper_cmd_length) :
+            end_index = len(upper_cmd)- (1 + i)
+            start_index = i
+            
+            if upper_cmd[start_index] != upper_cmd[end_index] :
+                return False
+
+        return True
